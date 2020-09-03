181dbafce6c37542061db8dd31bf07256bc4e79d
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-09-03 10:37:12 +0200

    Add clang coverage flags

--- scripts/cmake/Sanitizers.cmake
+++ scripts/cmake/Sanitizers.cmake
@@ -1,11 +1,16 @@
 # Enable sanitizers for a specific project
 function(enable_sanitizers project)
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
     option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
 
     if(ENABLE_COVERAGE)
       target_compile_options(project_options INTERFACE --coverage -O0 -g)
       target_link_libraries(project_options INTERFACE --coverage)
+
+      if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+        target_compile_options(project_options INTERFACE -fprofile-instr-generate -fcoverage-mapping)
+        target_link_libraries(project_options INTERFACE -fprofile-instr-generate -fcoverage-mapping)
+      endif()
     endif()
 
     set(sanitizers "")
