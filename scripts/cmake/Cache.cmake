e886fb4a4bff17053b31c0a3dc0780f90ded7f3e
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-09-03 12:05:35 +0200

    Add auto option to compiler caches

--- scripts/cmake/Cache.cmake
+++ scripts/cmake/Cache.cmake
@@ -4,9 +4,9 @@ if(NOT ENABLE_CACHE)
 endif()
 
 set(CACHE_OPTION
-    "ccache"
+    "auto"
     CACHE STRING "Compiler cache to be used")
-set(CACHE_OPTION_VALUES "ccache" "sccache")
+set(CACHE_OPTION_VALUES "auto" "ccache" "sccache")
 set_property(CACHE CACHE_OPTION PROPERTY STRINGS ${CACHE_OPTION_VALUES})
 list(FIND CACHE_OPTION_VALUES ${CACHE_OPTION} CACHE_OPTION_INDEX)
 
@@ -15,13 +15,41 @@ if(${CACHE_OPTION_INDEX} EQUAL -1)
                  "explicitly supported entries are ${CACHE_OPTION_VALUES}")
 endif()
 
-find_program(CACHE_BINARY ${CACHE_OPTION})
-if(CACHE_BINARY)
-  message(STATUS "${CACHE_OPTION} found and enabled")
-  set(CMAKE_CXX_COMPILER_LAUNCHER ${CACHE_BINARY})
-else()
-  message(WARNING "${CACHE_OPTION} is enabled but was not found. Disabling it.")
-  set(ENABLE_CACHE
+if(${CACHE_OPTION} STREQUAL "auto")
+  foreach(option ${CACHE_OPTION_VALUES})
+    if(NOT ${option} STREQUAL "auto")
+      message(STATUS "Testing cache compiler ${option}...")
+
+      find_program(CACHE_BINARY ${option})
+      if(CACHE_BINARY)
+        message(STATUS "Testing cache compiler ${option} - works")
+        set(CACHE_OPTION
+          ${option}
+          CACHE STRING "Compiler cache to be used" FORCE)
+        set(CMAKE_CXX_COMPILER_LAUNCHER ${CACHE_BINARY})
+        break()
+      else()
+        message(STATUS "Testing cache compiler ${option} - not working")
+      endif()
+    endif()
+  endforeach()
+
+  # Cache option is still auto means we coudn't find any suitable cache compiler
+  if(${CACHE_OPTION} STREQUAL "auto")
+    message(STATUS "No suitable compiler cache was found. Disabling caching")
+    set(ENABLE_CACHE
       OFF
       CACHE BOOL "Enable cache if available" FORCE)
+  endif()
+else()
+  find_program(CACHE_BINARY ${CACHE_OPTION})
+  if(CACHE_BINARY)
+    message(STATUS "${CACHE_OPTION} found and enabled")
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${CACHE_BINARY})
+  else()
+    message(WARNING "${CACHE_OPTION} is enabled but was not found. Disabling it.")
+    set(ENABLE_CACHE
+      OFF
+      CACHE BOOL "Enable cache if available" FORCE)
+  endif()
 endif()
