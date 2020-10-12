70514b026ff16141294c632b6fade1530fb5739f
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-10-12 16:18:55 +0200

    Fix versioning test and made it constexpr

--- tests/unittests/src/Config/Versioning.test.cpp
+++ tests/unittests/src/Config/Versioning.test.cpp
@@ -1,30 +1,43 @@
 #include <catch2/catch.hpp>
 
+#include "ConstexprHelper.hpp"
 #include <Phi/Config/Versioning.hpp>
 
 TEST_CASE("Versioning", "[Config][Versioning]")
 {
-    int val = PHI_VERSION_CREATE(3, 1, 4);
+    SECTION("Random values")
+    {
+        CONSTEXPR_RUNTIME int val = PHI_VERSION_CREATE(3, 1, 4);
 
-    CHECK(PHI_VERSION_EXTRACT_MAJOR(val) == 3);
-    CHECK(PHI_VERSION_EXTRACT_MINOR(val) == 1);
-    CHECK(PHI_VERSION_EXTRACT_PATCH(val) == 4);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MAJOR(val) == 3);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MINOR(val) == 1);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_PATCH(val) == 4);
+    }
 
-    val = PHI_VERSION_CREATE(0, 0, 0);
+    SECTION("Zero")
+    {
+        CONSTEXPR_RUNTIME int val = PHI_VERSION_CREATE(0, 0, 0);
 
-    CHECK(PHI_VERSION_EXTRACT_MAJOR(val) == 0);
-    CHECK(PHI_VERSION_EXTRACT_MINOR(val) == 0);
-    CHECK(PHI_VERSION_EXTRACT_PATCH(val) == 0);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MAJOR(val) == 0);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MINOR(val) == 0);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_PATCH(val) == 0);
+    }
 
-    val = PHI_VERSION_CREATE(2, 55, 13);
+    SECTION("More random values")
+    {
+        CONSTEXPR_RUNTIME int val = PHI_VERSION_CREATE(2, 55, 13);
 
-    CHECK(PHI_VERSION_EXTRACT_MAJOR(val) == 2);
-    CHECK(PHI_VERSION_EXTRACT_MINOR(val) == 55);
-    CHECK(PHI_VERSION_EXTRACT_PATCH(val) == 13);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MAJOR(val) == 2);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MINOR(val) == 55);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_PATCH(val) == 13);
+    }
 
-    val = PHI_VERSION_CREATE(255, 255, 255);
+    SECTION("Max values")
+    {
+        CONSTEXPR_RUNTIME int val = PHI_VERSION_CREATE(126, 126, 126);
 
-    CHECK(PHI_VERSION_EXTRACT_MAJOR(val) == 255);
-    CHECK(PHI_VERSION_EXTRACT_MINOR(val) == 255);
-    CHECK(PHI_VERSION_EXTRACT_PATCH(val) == 255);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MAJOR(val) == 126);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_MINOR(val) == 126);
+        STATIC_REQUIRE(PHI_VERSION_EXTRACT_PATCH(val) == 126);
+    }
 }
