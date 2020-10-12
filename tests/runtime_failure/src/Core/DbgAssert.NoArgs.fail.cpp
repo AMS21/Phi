2bf0e7d9323c77e603fa53a4726626f68428d8ee
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-10-12 16:05:04 +0200

    Silence warning about zero variadic macro extensions

--- tests/runtime_failure/src/Core/DbgAssert.NoArgs.fail.cpp
+++ tests/runtime_failure/src/Core/DbgAssert.NoArgs.fail.cpp
@@ -1,4 +1,5 @@
 #include "SetupDebugReportMode.hpp"
+#include <Phi/Config/Warning.hpp>
 #include <Phi/Core/Assert.hpp>
 #include <Phi/Core/Log.hpp>
 
@@ -8,5 +9,7 @@ int main()
 
     phi::Log::initialize_default_loggers();
 
+    PHI_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wgnu-zero-variadic-macro-arguments")
     PHI_DBG_ASSERT(false);
+    PHI_CLANG_SUPPRESS_WARNING_POP()
 }
