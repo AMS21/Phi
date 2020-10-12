80ee65590dd0d38cf6c6063cef4c3c384d65f2a0
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-10-12 14:20:36 +0200

    Fix even more warnings about unused variables

--- tests/unittests/src/Config/FunctionLikeMacro.test.cpp
+++ tests/unittests/src/Config/FunctionLikeMacro.test.cpp
@@ -5,13 +5,20 @@
 #define FN_TEST()                                                                                  \
     PHI_BEGIN_MACRO()                                                                              \
     int i = 3;                                                                                     \
+    j     = i;                                                                                     \
     PHI_END_MACRO()
 
 #define FN_TEST_2() PHI_EMPTY_MACRO()
 
 TEST_CASE("FunctionLikeMacro", "[Config][FunctionLikeMacro]")
 {
+    int j = 0;
+
+    REQUIRE(j == 0);
+
     FN_TEST();
+
+    CHECK(j == 3);
 
     FN_TEST_2();
 
