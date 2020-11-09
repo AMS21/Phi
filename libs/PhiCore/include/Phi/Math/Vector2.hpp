601bb6bd412b9dc2b7195a5ea91c7c74abf12f1d
AMS21 <19614997+AMS21@users.noreply.github.com>
2020-11-09 13:40:05 +0100

    Add and test Vector2 typedefs

--- libs/PhiCore/include/Phi/Math/Vector2.hpp
+++ libs/PhiCore/include/Phi/Math/Vector2.hpp
@@ -4,8 +4,8 @@
 #include "Phi/PhiConfig.hpp"
 
 #include "Phi/Core/Types.hpp"
-#include <utility>
 #include <iterator>
+#include <utility>
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
@@ -13,50 +13,49 @@ template <typename TypeT>
 class Vector2
 {
 public:
-    using this_type = Vector2<TypeT>;
-    using value_type = TypeT;
-    using reference =  value_type&;
-    using const_reference = const value_type&;
-    using pointer = value_type*;
-    using const_pointer = const value_type*;
-    using iterator = value_type*;
-    using const_iterator = const value_type*;
-    using reverse_iterator = std::reverse_iterator<iterator>;
+    using this_type              = Vector2<TypeT>;
+    using value_type             = TypeT;
+    using reference              = value_type&;
+    using const_reference        = const value_type&;
+    using pointer                = value_type*;
+    using const_pointer          = const value_type*;
+    using iterator               = value_type*;
+    using const_iterator         = const value_type*;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 public:
-
     // Constructors
-    constexpr Vector2(TypeT val_x, TypeT val_y) noexcept :
-        x(val_x),
-        y(val_y)
+    constexpr Vector2(TypeT val_x, TypeT val_y) noexcept
+        : x(val_x)
+        , y(val_y)
     {}
 
-    constexpr Vector2(TypeT val_xy) noexcept :
-        x(val_xy),
-        y(val_xy)
+    constexpr Vector2(TypeT val_xy) noexcept
+        : x(val_xy)
+        , y(val_xy)
     {}
 
-    constexpr Vector2(const Vector2<TypeT>& other) noexcept :
-        x(other.x),
-        y(other.y)
+    constexpr Vector2(const Vector2<TypeT>& other) noexcept
+        : x(other.x)
+        , y(other.y)
     {}
 
-    constexpr Vector2(Vector2<TypeT>&& other) noexcept :
-        x(std::move(other.x)),
-        y(std::move(other.y))
-    {}
-
-    template <typename OtherT>
-    constexpr Vector2(const Vector2<OtherT>& other) noexcept :
-        x(static_cast<TypeT>(other.x)),
-        y(static_cast<TypeT>(other.y))
+    constexpr Vector2(Vector2<TypeT>&& other) noexcept
+        : x(std::move(other.x))
+        , y(std::move(other.y))
     {}
 
     template <typename OtherT>
-    constexpr Vector2(Vector2<OtherT>&& other) noexcept :
-        x(static_cast<TypeT>(std::move(other.x))),
-        y(static_cast<TypeT>(std::move(other.y)))
+    constexpr Vector2(const Vector2<OtherT>& other) noexcept
+        : x(static_cast<TypeT>(other.x))
+        , y(static_cast<TypeT>(other.y))
+    {}
+
+    template <typename OtherT>
+    constexpr Vector2(Vector2<OtherT>&& other) noexcept
+        : x(static_cast<TypeT>(std::move(other.x)))
+        , y(static_cast<TypeT>(std::move(other.y)))
     {}
 
     // Operators
@@ -139,10 +138,22 @@ constexpr auto operator/(const Vector2<LhsT>& lhs, const RhsT& rhs) noexcept
 }
 
 // Define the most common types
-using Vector2i32 = Vector2<i32>;
-using Vector2u32 = Vector2<u32>;
-using Vector2f32 = Vector2<f32>;
-using Vector2f64 = Vector2<f64>;
+using Vector2i8    = Vector2<i8>;
+using Vector2u8    = Vector2<u8>;
+using Vector2i16   = Vector2<i16>;
+using Vector2u16   = Vector2<u16>;
+using Vector2i32   = Vector2<i32>;
+using Vector2u32   = Vector2<u32>;
+using Vector2i64   = Vector2<i64>;
+using Vector2u64   = Vector2<u64>;
+using Vector2imax  = Vector2<imax>;
+using Vector2umax  = Vector2<umax>;
+using Vector2isize = Vector2<isize>;
+using Vector2usize = Vector2<usize>;
+
+using Vector2f32  = Vector2<f32>;
+using Vector2f64  = Vector2<f64>;
+using Vector2fmax = Vector2<fmax>;
 
 DETAIL_PHI_END_NAMESPACE()
 
--- tests/unittests/src/Math/Vector2.test.cpp
+++ tests/unittests/src/Math/Vector2.test.cpp
@@ -1,17 +1,23 @@
 #include <catch2/catch_template_test_macros.hpp>
 
-#include <Phi/Math/Vector2.hpp>
-#include <Phi/Core/Integer.hpp>
-#include <Phi/Core/FloatingPoint.hpp>
-#include <Phi/Core/Types.hpp>
-#include <Phi/Config/Warning.hpp>
-#include "Unwrap.hpp"
 #include "ConstexprHelper.hpp"
+#include "Unwrap.hpp"
+#include <Phi/Config/Warning.hpp>
+#include <Phi/Core/FloatingPoint.hpp>
+#include <Phi/Core/Integer.hpp>
+#include <Phi/Core/Types.hpp>
+#include <Phi/Math/Vector2.hpp>
+#include <iterator>
+#include <type_traits>
 
 PHI_CLANG_SUPPRESS_WARNING_WITH_PUSH("-Wfloat-equal")
 PHI_GCC_SUPPRESS_WARNING_WITH_PUSH("-Wfloat-equal")
 
-TEMPLATE_TEST_CASE("Vector2 templated", "[Math][Vector2]", char, signed char, unsigned char, short, unsigned short, int, unsigned, long, unsigned long, long long, unsigned long long, float, double, long double, phi::i8, phi::i16, phi::i32, phi::i64, phi::u8, phi::u16, phi::u32, phi::u64, phi::FloatingPoint<float>, phi::FloatingPoint<double>, phi::FloatingPoint<long double>)
+TEMPLATE_TEST_CASE("Vector2 templated", "[Math][Vector2]", char, signed char, unsigned char, short,
+                   unsigned short, int, unsigned, long, unsigned long, long long,
+                   unsigned long long, float, double, long double, phi::i8, phi::i16, phi::i32,
+                   phi::i64, phi::u8, phi::u16, phi::u32, phi::u64, phi::FloatingPoint<float>,
+                   phi::FloatingPoint<double>, phi::FloatingPoint<long double>)
 {
     using base_t = unwrapped_t<TestType>;
 
@@ -169,12 +175,14 @@ TEST_CASE("Vector2 fixed types", "[Math][Vector2]")
 
     SECTION("Vector2(Vector2<Other>&&)")
     {
-        CONSTEXPR_RUNTIME phi::Vector2<phi::i16> r1(phi::Vector2<phi::i8>(std::int8_t(11), std::int8_t(17)));
+        CONSTEXPR_RUNTIME phi::Vector2<phi::i16> r1(
+                phi::Vector2<phi::i8>(std::int8_t(11), std::int8_t(17)));
 
         STATIC_REQUIRE(bool(r1.x == std::int16_t(11)));
         STATIC_REQUIRE(bool(r1.y == std::int16_t(17)));
 
-        CONSTEXPR_RUNTIME phi::Vector2<phi::i32> r2(phi::Vector2<phi::u16>(std::uint16_t(4u), std::uint16_t(1u)));
+        CONSTEXPR_RUNTIME phi::Vector2<phi::i32> r2(
+                phi::Vector2<phi::u16>(std::uint16_t(4u), std::uint16_t(1u)));
 
         STATIC_REQUIRE(bool(r2.x == std::int32_t(4)));
         STATIC_REQUIRE(bool(r2.y == std::int32_t(1)));
@@ -210,3 +218,48 @@ TEST_CASE("Vector2 fixed types", "[Math][Vector2]")
 
 PHI_GCC_SUPPRESS_WARNING_POP()
 PHI_CLANG_SUPPRESS_WARNING_POP()
+
+TEMPLATE_TEST_CASE("Vector2 typedefs", "[Math][Vector2]", char, signed char, unsigned char, short,
+                   unsigned short, int, unsigned, long, unsigned long, long long,
+                   unsigned long long, float, double, long double, phi::i8, phi::i16, phi::i32,
+                   phi::i64, phi::u8, phi::u16, phi::u32, phi::u64, phi::FloatingPoint<float>,
+                   phi::FloatingPoint<double>, phi::FloatingPoint<long double>)
+{
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::this_type, phi::Vector2<TestType>>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::value_type, TestType>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::reference, TestType&>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::const_reference, const TestType&>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::pointer, TestType*>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::const_pointer, const TestType*>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::iterator, TestType*>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::const_iterator, const TestType*>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::reverse_iterator,
+                                  std::reverse_iterator<TestType*>>);
+    STATIC_REQUIRE(std::is_same_v<phi::Vector2<TestType>::const_reverse_iterator,
+                                  std::reverse_iterator<const TestType*>>);
+}
+
+#define TEST_VEC2_TYPEDEF(vec_t, base_t)                                                           \
+    STATIC_REQUIRE(sizeof(vec_t) == (sizeof(base_t) * 2));                                         \
+    STATIC_REQUIRE(std::is_same_v<vec_t::value_type, base_t>)
+
+TEST_CASE("Vector2 global typedefs", "[Math][Vector2]")
+{
+    // global typedefs
+    TEST_VEC2_TYPEDEF(phi::Vector2i8, phi::i8);
+    TEST_VEC2_TYPEDEF(phi::Vector2u8, phi::u8);
+    TEST_VEC2_TYPEDEF(phi::Vector2i16, phi::i16);
+    TEST_VEC2_TYPEDEF(phi::Vector2u16, phi::u16);
+    TEST_VEC2_TYPEDEF(phi::Vector2i32, phi::i32);
+    TEST_VEC2_TYPEDEF(phi::Vector2u32, phi::u32);
+    TEST_VEC2_TYPEDEF(phi::Vector2i64, phi::i64);
+    TEST_VEC2_TYPEDEF(phi::Vector2u64, phi::u64);
+    TEST_VEC2_TYPEDEF(phi::Vector2imax, phi::imax);
+    TEST_VEC2_TYPEDEF(phi::Vector2umax, phi::umax);
+    TEST_VEC2_TYPEDEF(phi::Vector2isize, phi::isize);
+    TEST_VEC2_TYPEDEF(phi::Vector2usize, phi::usize);
+
+    TEST_VEC2_TYPEDEF(phi::Vector2f32, phi::f32);
+    TEST_VEC2_TYPEDEF(phi::Vector2f64, phi::f64);
+    TEST_VEC2_TYPEDEF(phi::Vector2fmax, phi::fmax);
+}
