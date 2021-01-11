9e1bd3ff4b1be996a0f6e9862dc6103e96d97c6d
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-01-11 17:36:52 +0100

    [Config] Implement PHI_UNREACHBLE
    Used to mark code as being unreachable.

---
+++ libs/PhiCore/include/Phi/Config/Unreachable.hpp
@@ -0,0 +1,22 @@
+#ifndef INCG_PHI_CONFIG_UNREACHABLE_HPP
+#define INCG_PHI_CONFIG_UNREACHABLE_HPP
+
+#include "Phi/Config/Assume.hpp"
+#include "Phi/Config/Compiler.hpp"
+#include "Phi/Config/FunctionLikeMacro.hpp"
+
+#if PHI_COMPILER_IS(MSVC)
+#    define PHI_UNREACHABLE() PHI_ASSUME(0)
+#elif PHI_COMPILER_IS(CLANG)
+#    if __has_builtin(__builtin_unreachable)
+#        define PHI_UNREACHABLE() __builtin_unreachable()
+#    else
+#        define PHI_UNREACHABLE() PHI_ASSUME(0)
+#    endif
+#elif PHI_COMPILER_IS_ATLEAST(GCC, 4, 5, 3)
+#    define PHI_UNREACHABLE() __builtin_unreachable()
+#else
+#    define PHI_UNREACHABLE() PHI_EMPTY_MACRO() /* Nothing */
+#endif
+
+#endif // INCG_PHI_CONFIG_UNREACHABLE_HPP
