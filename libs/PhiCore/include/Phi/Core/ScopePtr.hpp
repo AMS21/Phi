bbedb18b46014d8123b5d56de2272102b3c557c7
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-01-12 19:50:53 +0100

    [Core] Add NotNullScopePtr
    Same as the ScopePtr but with special checks to make sure you not
    assign a nullptr.

--- libs/PhiCore/include/Phi/Core/ScopePtr.hpp
+++ libs/PhiCore/include/Phi/Core/ScopePtr.hpp
@@ -13,10 +13,14 @@
 DETAIL_PHI_BEGIN_NAMESPACE()
 
 template <typename TypeT>
+class NotNullScopePtr;
+
+template <typename TypeT>
 class ScopePtr
 {
 public:
     using this_type       = ScopePtr<TypeT>;
+    using not_null_type   = NotNullScopePtr<TypeT>;
     using value_type      = TypeT;
     using reference       = TypeT&;
     using const_reference = const TypeT&;
@@ -44,6 +48,19 @@ public:
         : m_Ptr(other.leak_ptr())
     {}
 
+    constexpr ScopePtr(NotNullScopePtr<TypeT>&& other) noexcept
+        : m_Ptr(other.leak_ptr())
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Trying to assign from moved NotNullScopePtr");
+    }
+
+    template <typename OtherT, std::enable_if_t<std::is_convertible_v<OtherT*, TypeT*>> = 0>
+    constexpr ScopePtr(NotNullScopePtr<OtherT>&& other) noexcept
+        : m_Ptr(other.leak_ptr())
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Trying to assign from moved NotNullScopePtr");
+    }
+
     ~ScopePtr() noexcept
     {
         clear();
@@ -59,6 +76,25 @@ public:
     template <typename OtherT, std::enable_if_t<std::is_convertible_v<OtherT*, TypeT*>> = 0>
     constexpr ScopePtr<TypeT>& operator=(ScopePtr<OtherT>&& other) noexcept
     {
+        reset(other.leak_ptr());
+
+        return *this;
+    }
+
+    constexpr ScopePtr<TypeT>& operator=(NotNullScopePtr<TypeT>&& other) noexcept
+    {
+        PHI_DBG_ASSERT(other.get() != nullptr, "Trying to assign from moved NotNullScopePtr");
+
+        reset(other.leak_ptr());
+
+        return *this;
+    }
+
+    template <typename OtherT, std::enable_if_t<std::is_convertible_v<OtherT*, TypeT*>> = 0>
+    constexpr ScopePtr<TypeT>& operator=(NotNullScopePtr<OtherT>&& other) noexcept
+    {
+        PHI_DBG_ASSERT(other.get() != nullptr, "Trying to assign from moved NotNullScopePtr");
+
         reset(other.leak_ptr());
 
         return *this;
@@ -88,7 +124,7 @@ public:
         m_Ptr = nullptr;
     }
 
-    TypeT* leak_ptr() noexcept
+    constexpr TypeT* leak_ptr() noexcept
     {
         TypeT* pointer = get();
         m_Ptr          = nullptr;
@@ -96,9 +132,16 @@ public:
         return pointer;
     }
 
+    constexpr NotNullScopePtr<TypeT> release_not_null() noexcept
+    {
+        PHI_DBG_ASSERT(get() != nullptr, "Can not release to NotNullScopePtr from nullptr");
+
+        return NotNullScopePtr<TypeT>(leak_ptr());
+    }
+
     void reset(TypeT* new_pointer) noexcept
     {
-        clear();
+        delete m_Ptr;
         m_Ptr = new_pointer;
     }
 
@@ -213,6 +256,202 @@ constexpr void swap(ScopePtr<LhsT>& lhs, ScopePtr<RhsT>& rhs)
     lhs.swap(rhs);
 }
 
+/* NotNullScopePtr */
+
+template <typename TypeT>
+class NotNullScopePtr
+{
+public:
+    using this_type       = NotNullScopePtr<TypeT>;
+    using value_type      = TypeT;
+    using reference       = TypeT&;
+    using const_reference = const TypeT&;
+    using pointer         = TypeT*;
+    using const_pointer   = const TypeT*;
+
+    NotNullScopePtr() = delete;
+
+    NotNullScopePtr(std::nullptr_t) = delete;
+
+    constexpr explicit NotNullScopePtr(TypeT* pointer) noexcept
+        : m_Ptr(pointer)
+    {
+        PHI_DBG_ASSERT(pointer != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+    }
+
+    constexpr NotNullScopePtr(NotNullScopePtr<TypeT>&& other) noexcept
+        : m_Ptr(other.leak_ptr())
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+    }
+
+    template <typename OtherT, std::enable_if_t<std::is_convertible_v<OtherT*, TypeT*>> = 0>
+    constexpr NotNullScopePtr(NotNullScopePtr<OtherT>&& other) noexcept
+        : m_Ptr(other.leak_ptr())
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+    }
+
+    ~NotNullScopePtr() noexcept
+    {
+        clear();
+    }
+
+    constexpr NotNullScopePtr<TypeT>& operator=(NotNullScopePtr<TypeT>&& other) noexcept
+    {
+        PHI_DBG_ASSERT(other.get() != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+
+        reset(other.leak_ptr());
+
+        return *this;
+    }
+
+    template <typename OtherT, std::enable_if_t<std::is_convertible_v<OtherT*, TypeT*>> = 0>
+    constexpr NotNullScopePtr<TypeT>& operator=(NotNullScopePtr<OtherT>&& other) noexcept
+    {
+        PHI_DBG_ASSERT(other.get() != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+
+        reset(other.leak_ptr());
+
+        return *this;
+    }
+
+    constexpr NotNullScopePtr<TypeT>& operator=(TypeT* pointer) noexcept
+    {
+        PHI_DBG_ASSERT(pointer != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+
+        if (m_Ptr != pointer)
+        {
+            delete m_Ptr;
+        }
+        m_Ptr = pointer;
+
+        return *this;
+    }
+
+    NotNullScopePtr<TypeT>& operator=(std::nullptr_t) = delete;
+
+    TypeT* leak_ptr() noexcept
+    {
+        return std::exchange(m_Ptr, nullptr);
+    }
+
+    void reset(TypeT* new_pointer) noexcept
+    {
+        PHI_DBG_ASSERT(new_pointer != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+
+        delete m_Ptr;
+        m_Ptr = new_pointer;
+    }
+
+    constexpr void swap(NotNullScopePtr<TypeT>& other) noexcept
+    {
+        PHI_DBG_ASSERT(other.get() != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+
+        std::swap(m_Ptr, other.m_Ptr);
+    }
+
+    template <typename OtherT, std::enable_if_t<std::is_convertible_v<OtherT*, TypeT*>> = 0>
+    constexpr void swap(NotNullScopePtr<OtherT>& other) noexcept
+    {
+        PHI_DBG_ASSERT(other.get() != nullptr, "Trying to assign nullptr to phi::NotNullScopePtr");
+
+        std::swap(m_Ptr, other.m_Ptr);
+    }
+
+    operator bool() = delete;
+
+    operator Boolean() = delete;
+
+    constexpr TypeT* get() noexcept
+    {
+        return m_Ptr;
+    }
+
+    constexpr const TypeT* get() const noexcept
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Accessing phi::NotNullScopePtr after it was deleted");
+
+        return m_Ptr;
+    }
+
+    constexpr explicit operator const TypeT*() const noexcept
+    {
+        return get();
+    }
+
+    constexpr explicit operator TypeT*() noexcept
+    {
+        return get();
+    }
+
+    TypeT* operator->()
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Cannot dereference a nullptr");
+        return get();
+    }
+
+    const TypeT* operator->() const
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Cannot dereference a nullptr");
+        return get();
+    }
+
+    TypeT& operator*()
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Cannot dereference a nullptr");
+        return *get();
+    }
+
+    const TypeT& operator*() const
+    {
+        PHI_DBG_ASSERT(m_Ptr != nullptr, "Cannot dereference a nullptr");
+        return *get();
+    }
+
+private:
+    void clear() noexcept
+    {
+        delete m_Ptr;
+        m_Ptr = nullptr;
+    }
+
+private:
+    TypeT* m_Ptr;
+};
+
+template <typename LhsT, typename RhsT>
+constexpr Boolean operator==(const NotNullScopePtr<LhsT>& lhs,
+                             const NotNullScopePtr<RhsT>& rhs) noexcept
+{
+    return lhs.get() == rhs.get();
+}
+
+template <typename LhsT>
+Boolean operator==(const NotNullScopePtr<LhsT>& lhs, std::nullptr_t) = delete;
+
+template <typename RhsT>
+Boolean operator==(std::nullptr_t, const NotNullScopePtr<RhsT>& rhs) = delete;
+
+template <typename LhsT, typename RhsT>
+constexpr Boolean operator!=(const NotNullScopePtr<LhsT>& lhs,
+                             const NotNullScopePtr<RhsT>& rhs) noexcept
+{
+    return lhs.get() != rhs.get();
+}
+
+template <typename LhsT>
+Boolean operator!=(const NotNullScopePtr<LhsT>& lhs, std::nullptr_t) = delete;
+
+template <typename RhsT>
+Boolean operator!=(std::nullptr_t, const NotNullScopePtr<RhsT>& rhs) = delete;
+
+template <typename LhsT, typename RhsT>
+constexpr void swap(NotNullScopePtr<LhsT>& lhs, NotNullScopePtr<RhsT>& rhs)
+{
+    lhs.swap(rhs);
+}
+
 // make functions
 
 template <typename TypeT, typename... ArgsT>
@@ -250,6 +489,42 @@ template <typename TypeT, typename... ArgsT>
 std::enable_if_t<is_bounded_array_v<TypeT>, ScopePtr<TypeT>> make_scope_for_overwrite(
         ArgsT&&... args) = delete;
 
+template <typename TypeT, typename... ArgsT>
+[[nodiscard]] std::enable_if_t<!std::is_array<TypeT>::value, NotNullScopePtr<TypeT>>
+make_not_null_scope(ArgsT&&... args)
+{
+    return NotNullScopePtr<TypeT>(new TypeT(std::forward<ArgsT>(args)...));
+}
+
+template <typename TypeT>
+[[nodiscard]] std::enable_if_t<is_unbounded_array_v<TypeT>, NotNullScopePtr<TypeT>>
+make_not_null_scope(phi::usize size)
+{
+    return NotNullScopePtr<TypeT>(new typename std::remove_extent<TypeT>::type[size.get()]());
+}
+
+template <typename TypeT, typename... ArgsT>
+std::enable_if_t<is_bounded_array_v<TypeT>, NotNullScopePtr<TypeT>> make_not_null_scope(
+        ArgsT&&... args) = delete;
+
+template <typename TypeT>
+[[nodiscard]] std::enable_if_t<!std::is_array<TypeT>::value, NotNullScopePtr<TypeT>>
+make_not_null_scope_for_overwrite()
+{
+    return NotNullScopePtr<TypeT>(new TypeT);
+}
+
+template <typename TypeT>
+[[nodiscard]] std::enable_if_t<is_unbounded_array_v<TypeT>, NotNullScopePtr<TypeT>>
+make_not_null_scope_for_overwrite(phi::usize size)
+{
+    return NotNullScopePtr<TypeT>(new typename std::remove_extent<TypeT>::type[size.get()]);
+}
+
+template <typename TypeT, typename... ArgsT>
+std::enable_if_t<is_bounded_array_v<TypeT>, NotNullScopePtr<TypeT>>
+make_not_null_scope_for_overwrite(ArgsT&&... args) = delete;
+
 DETAIL_PHI_END_NAMESPACE()
 
 namespace std
@@ -258,6 +533,15 @@ namespace std
     struct hash<phi::ScopePtr<TypeT>>
     {
         std::size_t operator()(phi::ScopePtr<TypeT> pointer) const noexcept
+        {
+            return std::hash<TypeT*>()(pointer.get());
+        }
+    };
+
+    template <typename TypeT>
+    struct hash<phi::NotNullScopePtr<TypeT>>
+    {
+        std::size_t operator()(phi::NotNullScopePtr<TypeT> pointer) const noexcept
         {
             return std::hash<TypeT*>()(pointer.get());
         }
--- tests/unittests/src/Core/ScopePtr.test.cpp
+++ tests/unittests/src/Core/ScopePtr.test.cpp
@@ -79,6 +79,17 @@ TEST_CASE("ScopePtr", "[Core][ScopePtr]")
         CHECK(ptr.get() == raw_ptr);
     }
 
+    SECTION("ScopePtr(NotNullScopePtr&&)")
+    {
+        int* raw_ptr = new int(33);
+
+        phi::NotNullScopePtr<int> not_null(raw_ptr);
+        phi::ScopePtr<int>        ptr(std::move(not_null));
+
+        REQUIRE(ptr);
+        CHECK(ptr.get() == raw_ptr);
+    }
+
     SECTION("operator=(ScopePtr&&)")
     {
         int* raw_ptr = new int(16);
@@ -103,6 +114,19 @@ TEST_CASE("ScopePtr", "[Core][ScopePtr]")
         CHECK(ptr2.get() == nullptr);
         CHECK(ptr);
         CHECK(ptr.get() == raw_ptr2);
+    }
+
+    SECTION("operator=(NotNullScopePtr&&)")
+    {
+        int* raw_ptr = new int(21);
+
+        phi::NotNullScopePtr<int> not_null(raw_ptr);
+        phi::ScopePtr<int>        ptr;
+
+        ptr = std::move(not_null);
+
+        REQUIRE(ptr);
+        CHECK(ptr.get() == raw_ptr);
     }
 
     SECTION("ScopePtr(TypeT*)")
@@ -163,6 +187,16 @@ TEST_CASE("ScopePtr", "[Core][ScopePtr]")
         CHECK(ptr.get() == nullptr);
 
         delete leaked_ptr;
+    }
+
+    SECTION("release_not_null")
+    {
+        int*               raw_ptr = new int(21);
+        phi::ScopePtr<int> ptr(raw_ptr);
+
+        phi::NotNullScopePtr<int> not_null = ptr.release_not_null();
+
+        CHECK(not_null.get() == raw_ptr);
     }
 
     SECTION("reset")
@@ -310,6 +344,226 @@ TEST_CASE("ScopePtr", "[Core][ScopePtr]")
         CHECK(*int_ptr2 == 3);
 
         phi::ScopePtr<phi::Vector2i32> vec2_ptr = phi::make_scope<phi::Vector2i32>(3, 12);
+
+        CHECK((vec2_ptr->x == 3));
+        CHECK((vec2_ptr->y == 12));
+    }
+}
+
+TEST_CASE("NotNullScopePtr", "[Core][ScopePtr][NotNullScopePtr]")
+{
+    SECTION("Layout")
+    {
+        STATIC_REQUIRE(sizeof(phi::NotNullScopePtr<int>) == sizeof(int*));
+
+        STATIC_REQUIRE_FALSE(std::is_default_constructible_v<phi::NotNullScopePtr<int>>);
+        STATIC_REQUIRE_FALSE(std::is_copy_constructible_v<phi::NotNullScopePtr<int>>);
+        STATIC_REQUIRE(std::is_move_constructible_v<phi::NotNullScopePtr<int>>);
+        STATIC_REQUIRE_FALSE(std::is_copy_assignable_v<phi::NotNullScopePtr<int>>);
+        STATIC_REQUIRE(std::is_move_assignable_v<phi::NotNullScopePtr<int>>);
+    }
+
+    SECTION("Type aliases")
+    {
+        STATIC_REQUIRE(
+                std::is_same_v<phi::NotNullScopePtr<int>::this_type, phi::NotNullScopePtr<int>>);
+        STATIC_REQUIRE(std::is_same_v<phi::NotNullScopePtr<int>::value_type, int>);
+        STATIC_REQUIRE(std::is_same_v<phi::NotNullScopePtr<int>::reference, int&>);
+        STATIC_REQUIRE(std::is_same_v<phi::NotNullScopePtr<int>::const_reference, const int&>);
+        STATIC_REQUIRE(std::is_same_v<phi::NotNullScopePtr<int>::pointer, int*>);
+        STATIC_REQUIRE(std::is_same_v<phi::NotNullScopePtr<int>::const_pointer, const int*>);
+    }
+
+    SECTION("NotNullScopePtr(TypeT*)")
+    {
+        int*                      raw_ptr = new int(23);
+        phi::NotNullScopePtr<int> ptr(raw_ptr);
+
+        CHECK(ptr.get() == raw_ptr);
+        CHECK(*ptr == 23);
+
+        *raw_ptr = 19;
+
+        CHECK(*ptr == 19);
+    }
+
+    SECTION("NotNullScopePtr(NotNullScopePtr&&)")
+    {
+        int* raw_ptr = new int(21);
+
+        phi::NotNullScopePtr<int> base(raw_ptr);
+        phi::NotNullScopePtr<int> ptr(std::move(base));
+
+        CHECK(ptr.get() == raw_ptr);
+    }
+
+    SECTION("operator=(NotNullScopePtr&&)")
+    {
+        int* raw_ptr = new int(16);
+
+        phi::NotNullScopePtr<int> base(raw_ptr);
+        phi::NotNullScopePtr<int> ptr(new int(12));
+
+        ptr = std::move(base);
+
+        CHECK(ptr.get() == raw_ptr);
+
+        int* raw_ptr2 = new int(12);
+
+        phi::NotNullScopePtr<int> ptr2(raw_ptr2);
+
+        ptr = std::move(ptr2);
+
+        CHECK(ptr2.get() == nullptr);
+        CHECK(ptr.get() == raw_ptr2);
+    }
+
+    SECTION("NotNullScopePtr(TypeT*)")
+    {
+        int* raw_ptr = new int(21);
+
+        phi::NotNullScopePtr<int> ptr(new int(23));
+
+        ptr = raw_ptr;
+
+        CHECK(ptr.get() == raw_ptr);
+
+        int* raw_ptr2 = new int(22);
+
+        ptr = raw_ptr2;
+
+        CHECK(ptr.get() == raw_ptr2);
+    }
+
+    SECTION("leak_ptr")
+    {
+        int*                      raw_ptr = new int(16);
+        phi::NotNullScopePtr<int> ptr(raw_ptr);
+
+        int* leaked_ptr = ptr.leak_ptr();
+
+        CHECK(leaked_ptr == raw_ptr);
+        CHECK(ptr.get() == nullptr);
+
+        delete leaked_ptr;
+    }
+
+    SECTION("reset")
+    {
+        phi::NotNullScopePtr<int> ptr(new int(99));
+        int*                      raw_ptr = new int(1);
+
+        ptr.reset(raw_ptr);
+
+        CHECK(ptr.get() == raw_ptr);
+    }
+
+    SECTION("swap")
+    {
+        int* raw_ptr1 = new int(1);
+        int* raw_ptr2 = new int(2);
+
+        phi::NotNullScopePtr<int> ptr1(raw_ptr1);
+        phi::NotNullScopePtr<int> ptr2(raw_ptr2);
+
+        ptr1.swap(ptr2);
+
+        CHECK(ptr1.get() == raw_ptr2);
+        CHECK(ptr2.get() == raw_ptr1);
+
+        phi::swap(ptr1, ptr2);
+
+        CHECK(ptr1.get() == raw_ptr1);
+        CHECK(ptr2.get() == raw_ptr2);
+    }
+
+    SECTION("operator TypeT*")
+    {
+        int*                      raw_ptr = new int(12);
+        phi::NotNullScopePtr<int> ptr(raw_ptr);
+
+        int* new_ptr = static_cast<int*>(ptr);
+
+        CHECK(new_ptr == raw_ptr);
+    }
+
+    SECTION("operator->")
+    {
+        phi::NotNullScopePtr<A> ptr(new A);
+
+        CHECK(ptr->f());
+    }
+
+    SECTION("operator*")
+    {
+        int*                      raw_ptr = new int(0);
+        phi::NotNullScopePtr<int> ptr(raw_ptr);
+
+        CHECK(*ptr == 0);
+
+        *raw_ptr = 12;
+
+        CHECK(*ptr == 12);
+
+        *ptr = 9;
+
+        CHECK(*ptr == 9);
+        CHECK(*raw_ptr == 9);
+    }
+
+    SECTION("operator==")
+    {
+        int* raw_ptr1 = new int(1);
+        int* raw_ptr2 = new int(2);
+        int* raw_ptr3 = new int(3);
+
+        phi::NotNullScopePtr<int> ptr1(raw_ptr1);
+        phi::NotNullScopePtr<int> ptr2(raw_ptr2);
+        phi::NotNullScopePtr<int> ptr3(raw_ptr3);
+
+        CHECK_FALSE(ptr1 == ptr2);
+        CHECK_FALSE(ptr1 == ptr3);
+        CHECK_FALSE(ptr2 == ptr3);
+
+        ptr2 = raw_ptr1;
+
+        CHECK(ptr1 == ptr2);
+
+        // Need to leak the pointer otherwise we get a double free
+        ptr2.leak_ptr();
+    }
+
+    SECTION("operator!=")
+    {
+        int* raw_ptr1 = new int(1);
+        int* raw_ptr2 = new int(2);
+        int* raw_ptr3 = new int(3);
+
+        phi::ScopePtr<int> ptr1(raw_ptr1);
+        phi::ScopePtr<int> ptr2(raw_ptr2);
+        phi::ScopePtr<int> ptr3(raw_ptr3);
+
+        CHECK(ptr1 != ptr2);
+        CHECK(ptr1 != ptr3);
+        CHECK(ptr2 != ptr3);
+
+        ptr2 = raw_ptr1;
+
+        CHECK_FALSE(ptr1 != ptr2);
+
+        // Need to leak the pointer otherwise we get a double free
+        ptr2.leak_ptr();
+    }
+
+    SECTION("make_scope")
+    {
+        phi::NotNullScopePtr<int> int_ptr  = phi::make_not_null_scope<int>();
+        phi::NotNullScopePtr<int> int_ptr2 = phi::make_not_null_scope<int>(3);
+
+        CHECK(*int_ptr2 == 3);
+
+        phi::NotNullScopePtr<phi::Vector2i32> vec2_ptr =
+                phi::make_not_null_scope<phi::Vector2i32>(3, 12);
 
         CHECK((vec2_ptr->x == 3));
         CHECK((vec2_ptr->y == 12));
