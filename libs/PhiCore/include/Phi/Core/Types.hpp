649a52ae0d7375da4a0d8fa870b17922686c386d
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-01-27 23:19:47 +0100

    [Core] Add fast integer types

--- libs/PhiCore/include/Phi/Core/Types.hpp
+++ libs/PhiCore/include/Phi/Core/Types.hpp
@@ -1,5 +1,5 @@
-#ifndef INCG_PHI_UTILITY_TYPES_HPP
-#define INCG_PHI_UTILITY_TYPES_HPP
+#ifndef INCG_PHI_CORE_TYPES_HPP
+#define INCG_PHI_CORE_TYPES_HPP
 
 #include "Phi/Core/Boolean.hpp"
 #include "Phi/Core/FloatingPoint.hpp"
@@ -11,14 +11,24 @@
 DETAIL_PHI_BEGIN_NAMESPACE()
 
 // Integer
-using i8    = Integer<std::int8_t>;
-using u8    = Integer<std::uint8_t>;
-using i16   = Integer<std::int16_t>;
-using u16   = Integer<std::uint16_t>;
-using i32   = Integer<std::int32_t>;
-using u32   = Integer<std::uint32_t>;
-using i64   = Integer<std::int64_t>;
-using u64   = Integer<std::uint64_t>;
+using i8  = Integer<std::int8_t>;
+using u8  = Integer<std::uint8_t>;
+using i16 = Integer<std::int16_t>;
+using u16 = Integer<std::uint16_t>;
+using i32 = Integer<std::int32_t>;
+using u32 = Integer<std::uint32_t>;
+using i64 = Integer<std::int64_t>;
+using u64 = Integer<std::uint64_t>;
+
+using i8_fast  = Integer<std::int_fast8_t>;
+using u8_fast  = Integer<std::uint_fast8_t>;
+using i16_fast = Integer<std::int_fast16_t>;
+using u16_fast = Integer<std::uint_fast16_t>;
+using i32_fast = Integer<std::int_fast32_t>;
+using u32_fast = Integer<std::uint_fast32_t>;
+using i64_fast = Integer<std::int_fast64_t>;
+using u64_fast = Integer<std::uint_fast64_t>;
+
 using isize = Integer<std::make_signed<std::size_t>::type>;
 using usize = Integer<std::size_t>;
 using imax  = i64;
@@ -31,4 +41,4 @@ using fmax = f64;
 
 DETAIL_PHI_END_NAMESPACE()
 
-#endif // INCG_PHI_UTILITY_TYPES_HPP
+#endif // INCG_PHI_CORE_TYPES_HPP
