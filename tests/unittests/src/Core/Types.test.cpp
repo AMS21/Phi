9ed29899b1b32b8121147c5c0a92fedff55c3a68
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-01-28 11:18:00 +0100

    [Core] Add tests for fast integer types

--- tests/unittests/src/Core/Types.test.cpp
+++ tests/unittests/src/Core/Types.test.cpp
@@ -34,6 +34,26 @@ TEST_CASE("Types", "[Utility][Types]")
         STATIC_REQUIRE(SIZE_IN_BITS(phi::u64) == 64);
         STATIC_REQUIRE(std::is_unsigned_v<phi::u64::value_type>);
 
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::i8_fast) >= 8);
+        STATIC_REQUIRE(std::is_signed_v<phi::i8_fast::value_type>);
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::u8_fast) >= 8);
+        STATIC_REQUIRE(std::is_unsigned_v<phi::u8_fast::value_type>);
+
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::i16_fast) >= 16);
+        STATIC_REQUIRE(std::is_signed_v<phi::i16_fast::value_type>);
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::u16_fast) >= 16);
+        STATIC_REQUIRE(std::is_unsigned_v<phi::u16_fast::value_type>);
+
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::i32_fast) >= 32);
+        STATIC_REQUIRE(std::is_signed_v<phi::i32_fast::value_type>);
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::u32_fast) >= 32);
+        STATIC_REQUIRE(std::is_unsigned_v<phi::u32_fast::value_type>);
+
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::i64_fast) >= 64);
+        STATIC_REQUIRE(std::is_signed_v<phi::i64_fast::value_type>);
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::u64_fast) >= 64);
+        STATIC_REQUIRE(std::is_unsigned_v<phi::u64_fast::value_type>);
+
         STATIC_REQUIRE(SIZE_IN_BITS(phi::isize) == SIZE_IN_BITS(phi::usize));
         STATIC_REQUIRE(std::is_signed_v<phi::isize::value_type>);
         STATIC_REQUIRE(std::is_unsigned_v<phi::usize::value_type>);
