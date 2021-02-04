be533fb8ea4a975e77dd9d208b9ad3546fc69d01
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-02-04 16:09:33 +0100

    [Core] Properly detect Emscripten Platform
    Since Emscripten also defined the unix macros we need to
    check it there.

--- libs/PhiCore/include/Phi/Config/Platform.hpp
+++ libs/PhiCore/include/Phi/Config/Platform.hpp
@@ -75,6 +75,10 @@
 #        define PHI_PLATFORM_BSD()     1
 #        define PHI_PLATFORM_OPENBSD() 1
 #        define PHI_PLATFORM_NAME()    "OpenBSD"
+#    elif defined(__EMSCRIPTEN__)
+#        undef PHI_PLATFORM_WEB
+#        define PHI_PLATFORM_WEB()  1
+#        define PHI_PLATFORM_NAME() "Web"
 #    else
 #        define PHI_PLATFORM_NAME() "Unknown"
 #        warning "Unknown Unix based operating system"
@@ -85,10 +89,6 @@
 #    define PHI_PLATFORM_UNIX()    1
 #    define PHI_PLATFORM_SOLARIS() 1
 #    define PHI_PLATFORM_NAME()    "Solaris"
-#elif defined(__EMSCRIPTEN__)
-#    undef PHI_PLATFORM_WEB
-#    define PHI_PLATFORM_WEB()  1
-#    define PHI_PLATFORM_NAME() "Web"
 #else
 #    define PHI_PLATFORM_NAME() "Unknown"
 #    warning "Platform could not be detected"
