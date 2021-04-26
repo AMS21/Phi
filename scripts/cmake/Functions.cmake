fb4efc3e0c98f96223efe002ca8cd655d8c45510
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-04-26 12:52:00 +0200

    [CMake] Chache variables from Environment.cmake
    This should allow us to only include the file once but use the
    variables everywhere.

--- scripts/cmake/Functions.cmake
+++ scripts/cmake/Functions.cmake
@@ -1,5 +1,7 @@
 include_guard(GLOBAL)
 
+include(CMakeParseArguments)
+
 # Log a trace message
 function(phi_trace text)
   message(STATUS "[Phi]: ${text}")
@@ -18,4 +20,22 @@ endfunction()
 # Give a configuration error
 function(phi_error text)
   message(FATAL_ERROR "[Phi]: Error: ${text}")
+endfunction()
+
+function(phi_set_cache_value)
+  # Commandline
+  cmake_parse_arguments(scv "" "NAME;VALUE" "" ${ARGN})
+
+  # Check required arguments
+  if(NOT DEFINED scv_NAME)
+    phi_error("phi_set_cache_value: NAME must be defined.")
+  endif()
+
+  if(NOT DEFINED scv_VALUE)
+    phi_error("phi_set_cache_value: scv_VALUE must be defined.")
+  endif()
+
+  set(${scv_NAME}
+      ${scv_VALUE}
+      CACHE INTERNAL "")
 endfunction()
