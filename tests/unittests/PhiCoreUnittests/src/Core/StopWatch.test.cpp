5bfe7a01e82e361ae55ed4336b5803dd184cd979
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-04-30 21:07:28 +0200

    [Core] Implement simple stop watch

---
+++ tests/unittests/PhiCoreUnittests/src/Core/StopWatch.test.cpp
@@ -0,0 +1,54 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <Phi/Core/StopWatch.hpp>
+#include <chrono>
+#include <ratio>
+#include <thread>
+
+TEST_CASE("StopWatch", "[Core][StopWatch]")
+{
+    SECTION("defaulted")
+    {
+        phi::StopWatch sw;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+
+        std::chrono::duration<double, std::milli> elapsed = sw.getElapsed();
+
+        CHECK(elapsed.count() >= 1);
+    }
+
+    SECTION("system clock")
+    {
+        phi::StopWatch<std::chrono::system_clock> sw;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+
+        std::chrono::duration<double, std::milli> elapsed = sw.getElapsed();
+
+        CHECK(elapsed.count() >= 1);
+    }
+
+    SECTION("steady clock")
+    {
+        phi::StopWatch<std::chrono::steady_clock> sw;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+
+        std::chrono::duration<double, std::milli> elapsed = sw.getElapsed();
+
+        CHECK(elapsed.count() >= 1);
+    }
+
+    SECTION("Elapsed cast")
+    {
+        phi::StopWatch<std::chrono::system_clock> sw;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+
+        std::chrono::duration<std::int32_t, std::micro> elapsed =
+                sw.getElapsed<std::int32_t, std::micro>();
+
+        CHECK(elapsed.count() >= 1);
+    }
+}
