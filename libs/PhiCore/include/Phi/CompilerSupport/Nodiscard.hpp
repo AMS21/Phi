421ebb288b269dbcfd9d2df4bba39af9d624d2cc
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-11-24 20:19:51 +0100

    [Config] Add PHI_NODISCARD_CLASS macro

--- libs/PhiCore/include/Phi/CompilerSupport/Nodiscard.hpp
+++ libs/PhiCore/include/Phi/CompilerSupport/Nodiscard.hpp
@@ -11,13 +11,17 @@
 #include "Phi/Config/Compiler.hpp"
 
 #if PHI_HAS_FEATURE_NODISCARD()
-#    define PHI_NODISCARD [[nodiscard]]
+#    define PHI_NODISCARD       [[nodiscard]]
+#    define PHI_NODISCARD_CLASS [[nodiscard]]
 #elif PHI_HAS_EXTENSION_ATTRIBUTE_WARN_UNUSED_RESULT()
-#    define PHI_NODISCARD __attribute__((warn_unused_result))
+#    define PHI_NODISCARD       __attribute__((warn_unused_result))
+#    define PHI_NODISCARD_CLASS __attribute__((warn_unused_result))
 #elif PHI_HAS_EXTENSION_CHECK_RETURN()
-#    define PHI_NODISCARD _Check_return_
+#    define PHI_NODISCARD       _Check_return_
+#    define PHI_NODISCARD_CLASS /* Nothing */
 #else
-#    define PHI_NODISCARD /* Nothing */
+#    define PHI_NODISCARD       /* Nothing */
+#    define PHI_NODISCARD_CLASS /* Nothing */
 #endif
 
 #endif // INCG_PHI_CORE_COMPILER_SUPPORT_NODISCARD_HPP
