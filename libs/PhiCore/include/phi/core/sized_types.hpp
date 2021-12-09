9bd4d0e2b3ea1bca6f428e2ab82d734ef970cc86
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-12-09 15:00:01 +0100

    [Core] Add sized_types.hpp

---
+++ libs/PhiCore/include/phi/core/sized_types.hpp
@@ -0,0 +1,199 @@
+#ifndef INCG_PHI_CORE_SIZED_TYPES_HPP
+#define INCG_PHI_CORE_SIZED_TYPES_HPP
+
+#include "phi/phi_config.hpp"
+
+#if PHI_HAS_EXTENSION_PRAGMA_ONCE()
+#    pragma once
+#endif
+
+#include "phi/compiler_support/type_system.hpp"
+#include <climits> // CHAR_BIT
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+namespace detail
+{
+    // int8
+#if PHI_TYPE_SYSTEM_SIZEOF_CHAR() * CHAR_BIT == 8
+    using signed_int8   = signed char;
+    using unsigned_int8 = unsigned char;
+#elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 8
+    using signed_int8                        = signed short;
+    using unsigned int8                      = unsigned short;
+#elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 8
+    using signed_int8                        = signed int;
+    using unsigned int8                      = unsigned int;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 8
+    using signed_int8                        = signed long;
+    using unsigned_int8                      = unsigned long;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 8
+    using signed_int8    = signed long long;
+    using unsigned_int8  = unsigned long long;
+#else
+#    error "No integer of size 8 found!"
+#endif
+
+    // int16
+#if PHI_TYPE_SYSTEM_SIZEOF_CHAR() * CHAR_BIT == 16
+    using signed_int16   = signed char;
+    using unsigned_int16 = unsigned char;
+#elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 16
+    using signed_int16                       = signed short;
+    using unsigned_int16                     = unsigned short;
+#elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 16
+    using signed_int16                       = signed int;
+    using unsigned int16                     = unsigned int;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 16
+    using signed_int16                       = signed long;
+    using unsigned_int16                     = unsigned long;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 16
+    using signed_int16   = signed long long;
+    using unsigned_int16 = unsigned long long;
+#else
+#    error "No integer of size 16 found!"
+#endif
+
+    // int32
+#if PHI_TYPE_SYSTEM_SIZEOF_CHAR() * CHAR_BIT == 32
+    using signed_int32   = signed char;
+    using unsigned_int32 = unsigned char;
+#elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 32
+    using signed_int32                       = signed short;
+    using unsigned_int32                     = unsigned short;
+#elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 32
+    using signed_int32                       = signed int;
+    using unsigned_int32                     = unsigned int;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 32
+    using signed_int32                       = signed long;
+    using unsigned_int32                     = unsigned long;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 32
+    using signed_int32   = signed long long;
+    using unsigned_int32 = unsigned long long;
+#else
+#    error "No integer of size 32 found!"
+#endif
+
+    // int64
+#if PHI_TYPE_SYSTEM_SIZEOF_CHAR() * CHAR_BIT == 64
+    using signed_int64   = signed char;
+    using unsigned_int64 = unsigned char;
+#elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 64
+    using signed_int64                       = signed short;
+    using unsigned_int64                     = unsigned short;
+#elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 64
+    using signed_int64                       = signed int;
+    using unsigned_int64                     = unsigned int;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 64
+    using signed_int64                       = signed long;
+    using unsigned_int64                     = unsigned long;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 64
+    using signed_int64   = signed long long;
+    using unsigned_int64 = unsigned long long;
+#else
+#    error "No integer of size 64 found!"
+#endif
+
+    // float32
+#if PHI_TYPE_SYSTEM_SIZEOF_FLOAT() * CHAR_BIT == 32
+    using float32 = float;
+#elif PHI_TYPE_SYSTEM_SIZEOF_DOUBLE() * CHAR_BIT == 32
+    using float32                            = double;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG_DOUBLE() * CHAR_BIT == 32
+    using float32                            = long double;
+#else
+#    error "No float of size 32 found!"
+#endif
+
+    // float64
+#if PHI_TYPE_SYSTEM_SIZEOF_FLOAT() * CHAR_BIT == 64
+    using float64 = float;
+#elif PHI_TYPE_SYSTEM_SIZEOF_DOUBLE() * CHAR_BIT == 64
+    using float64                            = double;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG_DOUBLE() * CHAR_BIT == 64
+    using float64                            = long double;
+#else
+#    error "No float of size 64 found!"
+#endif
+
+    // Optional float128
+#if PHI_TYPE_SYSTEM_SIZEOF_FLOAT() * CHAR_BIT == 128
+    static constexpr const bool has_float128 = true;
+    using float128                           = float;
+#elif PHI_TYPE_SYSTEM_SIZEOF_DOUBLE() * CHAR_BIT == 128
+    static constexpr const bool has_float128 = true;
+    using float128                           = double;
+#elif PHI_TYPE_SYSTEM_SIZEOF_LONG_DOUBLE() * CHAR_BIT == 128
+    static constexpr const bool has_float128 = true;
+    using float128                           = long double;
+#else
+    static constexpr const bool has_float128 = false;
+    using float128                           = void;
+#endif
+} // namespace detail
+
+using int8_t  = detail::signed_int8;
+using uint8_t = detail::unsigned_int8;
+
+using int16_t  = detail::signed_int16;
+using uint16_t = detail::unsigned_int16;
+
+using int32_t  = detail::signed_int32;
+using uint32_t = detail::unsigned_int32;
+
+using int64_t  = detail::signed_int64;
+using uint64_t = detail::unsigned_int64;
+
+using intmax_t  = int64_t;
+using uintmax_t = uint64_t;
+
+using int_fast8_t  = detail::signed_int8;
+using uint_fast8_t = detail::unsigned_int8;
+#if PHI_TYPE_SYSTEM_SIZEOF_VOIDPTR() * CHAR_BIT == 8
+using int_fast16_t  = detail::signed_int16;
+using uint_fast16_t = detail::unsigned_int16;
+using int_fast32_t  = detail::signed_int32;
+using uint_fast32_t = detail::unsigned_int32;
+using int_fast64_t  = detail::signed_int64;
+using uint_fast64_t = detail::unsigned_int64;
+#elif PHI_TYPE_SYSTEM_SIZEOF_VOIDPTR() * CHAR_BIT == 16
+using int_fast16_t = detail::signed_int16;
+using uint_fast16_t = detail::unsigned_int16;
+using int_fast32_t = detail::signed_int32;
+using uint_fast32_t = detail::unsigned_int32;
+using int_fast64_t = detail::signed_int64;
+using uint_fast64_t = detail::unsigned_int64;
+#elif PHI_TYPE_SYSTEM_SIZEOF_VOIDPTR() * CHAR_BIT == 32
+using int_fast16_t = detail::signed_int32;
+using uint_fast16_t = detail::unsigned_int32;
+using int_fast32_t = detail::signed_int32;
+using uint_fast32_t = detail::unsigned_int32;
+using int_fast64_t = detail::signed_int64;
+using uint_fast64_t = detail::unsigned_int64;
+#elif PHI_TYPE_SYSTEM_SIZEOF_VOIDPTR() * CHAR_BIT == 64
+using int_fast16_t = detail::signed_int64;
+using uint_fast16_t = detail::unsigned_int64;
+using int_fast32_t = detail::signed_int64;
+using uint_fast32_t = detail::unsigned_int64;
+using int_fast64_t = detail::signed_int64;
+using uint_fast64_t = detail::unsigned_int64;
+#endif
+
+using int_least8_t  = detail::signed_int8;
+using uint_least8_t = detail::unsigned_int8;
+
+using int_least16_t  = detail::signed_int16;
+using uint_least16_t = detail::unsigned_int16;
+
+using int_least32_t  = detail::signed_int32;
+using uint_least32_t = detail::unsigned_int32;
+
+using int_least64_t  = detail::signed_int64;
+using uint_least64_t = detail::unsigned_int64;
+
+using float32 = detail::float32;
+using float64 = detail::float64;
+
+DETAIL_PHI_END_NAMESPACE()
+
+#endif // INCG_PHI_CORE_SIZED_TYPES_HPP
---
+++ tests/PhiCore/unittests/src/core/sized_types.test.cpp
@@ -0,0 +1,222 @@
+#include <phi/test/test_macros.hpp>
+
+#include <phi/core/sized_types.hpp>
+#include <phi/type_traits/is_signed.hpp>
+#include <phi/type_traits/is_unsigned.hpp>
+#include <climits>
+#include <cstdint>
+
+#define SIZE_IN_BITS(x) (sizeof(x) * CHAR_BIT)
+
+TEST_CASE("sized_types")
+{
+    SECTION("int8_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int8_t) == 8);
+        STATIC_REQUIRE(phi::is_signed<phi::int8_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int8_t>::value);
+        CHECK_SAME_TYPE(phi::int8_t, std::int8_t);
+    }
+
+    SECTION("int16_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int16_t) == 16);
+        STATIC_REQUIRE(phi::is_signed<phi::int16_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int16_t>::value);
+        CHECK_SAME_TYPE(phi::int16_t, std::int16_t);
+    }
+
+    SECTION("int32_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int32_t) == 32);
+        STATIC_REQUIRE(phi::is_signed<phi::int32_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int32_t>::value);
+        CHECK_SAME_TYPE(phi::int32_t, std::int32_t);
+    }
+
+    SECTION("int64_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int64_t) == 64);
+        STATIC_REQUIRE(phi::is_signed<phi::int64_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int64_t>::value);
+        CHECK_SAME_TYPE(phi::int64_t, std::int64_t);
+    }
+
+    SECTION("intmax_t")
+    {
+        CHECK_SAME_TYPE(phi::intmax_t, phi::int64_t);
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::intmax_t) == 64);
+        STATIC_REQUIRE(phi::is_signed<phi::intmax_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::intmax_t>::value);
+        CHECK_SAME_TYPE(phi::intmax_t, std::intmax_t);
+    }
+
+    SECTION("uint8_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint8_t) == 8);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint8_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint8_t>::value);
+        CHECK_SAME_TYPE(phi::uint8_t, std::uint8_t);
+    }
+
+    SECTION("uint16_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint16_t) == 16);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint16_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint16_t>::value);
+        CHECK_SAME_TYPE(phi::uint16_t, std::uint16_t);
+    }
+
+    SECTION("uint32_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint32_t) == 32);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint32_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint32_t>::value);
+        CHECK_SAME_TYPE(phi::uint32_t, std::uint32_t);
+    }
+
+    SECTION("uint64_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint64_t) == 64);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint64_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint64_t>::value);
+        CHECK_SAME_TYPE(phi::uint64_t, std::uint64_t);
+    }
+
+    SECTION("uintmax_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uintmax_t) == 64);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uintmax_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uintmax_t>::value);
+        CHECK_SAME_TYPE(phi::uintmax_t, phi::uint64_t);
+        CHECK_SAME_TYPE(phi::uintmax_t, std::uintmax_t);
+    }
+
+    SECTION("int_fast8_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_fast8_t) >= 8);
+        STATIC_REQUIRE(phi::is_signed<phi::int_fast8_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_fast8_t>::value);
+        CHECK_SAME_TYPE(phi::int_fast8_t, std::int_fast8_t);
+    }
+
+    SECTION("int_fast16_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_fast16_t) >= 16);
+        STATIC_REQUIRE(phi::is_signed<phi::int_fast16_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_fast16_t>::value);
+        CHECK_SAME_TYPE(phi::int_fast16_t, std::int_fast16_t);
+    }
+
+    SECTION("int_fast32_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_fast32_t) >= 32);
+        STATIC_REQUIRE(phi::is_signed<phi::int_fast32_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_fast32_t>::value);
+        CHECK_SAME_TYPE(phi::int_fast32_t, std::int_fast32_t);
+    }
+
+    SECTION("int_fast64_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_fast64_t) >= 64);
+        STATIC_REQUIRE(phi::is_signed<phi::int_fast64_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_fast64_t>::value);
+        CHECK_SAME_TYPE(phi::int_fast64_t, std::int_fast64_t);
+    }
+
+    SECTION("uint_fast8_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_fast8_t) >= 8);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_fast8_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_fast8_t>::value);
+        CHECK_SAME_TYPE(phi::uint_fast8_t, std::uint_fast8_t);
+    }
+
+    SECTION("uint_fast16_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_fast16_t) >= 16);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_fast16_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_fast16_t>::value);
+        CHECK_SAME_TYPE(phi::uint_fast16_t, std::uint_fast16_t);
+    }
+
+    SECTION("uint_fast32_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_fast32_t) >= 32);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_fast32_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_fast32_t>::value);
+        CHECK_SAME_TYPE(phi::uint_fast32_t, std::uint_fast32_t);
+    }
+
+    SECTION("uint_fast64_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_fast64_t) >= 64);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_fast64_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_fast64_t>::value);
+        CHECK_SAME_TYPE(phi::uint_fast64_t, std::uint_fast64_t);
+    }
+
+    SECTION("int_least8_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_least8_t) >= 8);
+        STATIC_REQUIRE(phi::is_signed<phi::int_least8_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_least8_t>::value);
+        CHECK_SAME_TYPE(phi::int_least8_t, std::int_least8_t);
+    }
+
+    SECTION("int_least16_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_least16_t) >= 16);
+        STATIC_REQUIRE(phi::is_signed<phi::int_least16_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_least16_t>::value);
+        CHECK_SAME_TYPE(phi::int_least16_t, std::int_least16_t);
+    }
+
+    SECTION("int_least32_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_least32_t) >= 32);
+        STATIC_REQUIRE(phi::is_signed<phi::int_least32_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_least32_t>::value);
+        CHECK_SAME_TYPE(phi::int_least32_t, std::int_least32_t);
+    }
+
+    SECTION("int_least64_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::int_least64_t) >= 64);
+        STATIC_REQUIRE(phi::is_signed<phi::int_least64_t>::value);
+        STATIC_REQUIRE_FALSE(phi::is_unsigned<phi::int_least64_t>::value);
+        CHECK_SAME_TYPE(phi::int_least64_t, std::int_least64_t);
+    }
+
+    SECTION("uint_least8_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_least8_t) >= 8);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_least8_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_least8_t>::value);
+        CHECK_SAME_TYPE(phi::uint_least8_t, std::uint_least8_t);
+    }
+
+    SECTION("uint_least16_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_least16_t) >= 16);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_least16_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_least16_t>::value);
+        CHECK_SAME_TYPE(phi::uint_least16_t, std::uint_least16_t);
+    }
+
+    SECTION("uint_least32_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_least32_t) >= 32);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_least32_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_least32_t>::value);
+        CHECK_SAME_TYPE(phi::uint_least32_t, std::uint_least32_t);
+    }
+
+    SECTION("uint_least64_t")
+    {
+        STATIC_REQUIRE(SIZE_IN_BITS(phi::uint_least64_t) >= 64);
+        STATIC_REQUIRE_FALSE(phi::is_signed<phi::uint_least64_t>::value);
+        STATIC_REQUIRE(phi::is_unsigned<phi::uint_least64_t>::value);
+        CHECK_SAME_TYPE(phi::uint_least64_t, std::uint_least64_t);
+    }
+}
