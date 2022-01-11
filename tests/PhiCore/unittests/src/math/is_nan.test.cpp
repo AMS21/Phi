17a9434f80cc33e6446db6baa333af1aba23de21
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-01-11 12:34:57 +0100

    [tests] Improve tests for is_nan

--- tests/PhiCore/unittests/src/math/is_nan.test.cpp
+++ tests/PhiCore/unittests/src/math/is_nan.test.cpp
@@ -18,6 +18,18 @@ void test_is_nan(T val)
     CHECK(std::isnan(phi::to_unsafe(val)));
 }
 
+#define STATIC_TEST_IS_NAN(val)                                                                    \
+    STATIC_REQUIRE(phi::is_nan(val));                                                              \
+    CHECK_NOEXCEPT(phi::is_nan(val));                                                              \
+    /* Standard compatibility */                                                                   \
+    CHECK(std::isnan(phi::to_unsafe(val)))
+
+#define STATIC_TEST_IS_NOT_NAN(val)                                                                \
+    STATIC_REQUIRE_FALSE(phi::is_nan(val));                                                        \
+    CHECK_NOEXCEPT(phi::is_nan(val));                                                              \
+    /* Standard compatibility */                                                                   \
+    CHECK_FALSE(std::isnan(phi::to_unsafe(val)))
+
 using sf = phi::floating_point<float>;
 using sd = phi::floating_point<double>;
 using sl = phi::floating_point<long double>;
@@ -47,27 +59,27 @@ TEST_CASE("is_nan")
 #if PHI_PLATFORM_IS(WINDOWS)
     test_is_nan(NAN);
 #else
-    STATIC_REQUIRE(phi::is_nan(NAN));
+    STATIC_TEST_IS_NAN(NAN);
 #endif
-    STATIC_REQUIRE(phi::is_nan(std::numeric_limits<float>::quiet_NaN()));
-    STATIC_REQUIRE(phi::is_nan(std::numeric_limits<float>::signaling_NaN()));
+    STATIC_TEST_IS_NAN(std::numeric_limits<float>::quiet_NaN());
+    STATIC_TEST_IS_NAN(std::numeric_limits<float>::signaling_NaN());
 
-    STATIC_REQUIRE_FALSE(phi::is_nan(0.0f));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-0.0f));
-    STATIC_REQUIRE_FALSE(phi::is_nan(1.0f));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-1.0f));
-    STATIC_REQUIRE_FALSE(phi::is_nan(INFINITY));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-INFINITY));
-    STATIC_REQUIRE_FALSE(phi::is_nan(HUGE_VALF));
-    STATIC_REQUIRE_FALSE(phi::is_nan(FLT_MIN / 2.0f));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<float>::epsilon()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<float>::infinity()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-std::numeric_limits<float>::infinity()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<float>::min()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<float>::max()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<float>::lowest()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<float>::denorm_min()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<float>::round_error()));
+    STATIC_TEST_IS_NOT_NAN(0.0f);
+    STATIC_TEST_IS_NOT_NAN(-0.0f);
+    STATIC_TEST_IS_NOT_NAN(1.0f);
+    STATIC_TEST_IS_NOT_NAN(-1.0f);
+    STATIC_TEST_IS_NOT_NAN(INFINITY);
+    STATIC_TEST_IS_NOT_NAN(-INFINITY);
+    STATIC_TEST_IS_NOT_NAN(HUGE_VALF);
+    STATIC_TEST_IS_NOT_NAN(FLT_MIN / 2.0f);
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<float>::epsilon());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<float>::infinity());
+    STATIC_TEST_IS_NOT_NAN(-std::numeric_limits<float>::infinity());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<float>::min());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<float>::max());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<float>::lowest());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<float>::denorm_min());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<float>::round_error());
 
     // double
     test_is_nan(std::nan(""));
@@ -78,23 +90,23 @@ TEST_CASE("is_nan")
     test_is_nan(0.0 / 0.0);
 #endif
     test_is_nan(std::numeric_limits<double>::infinity() * 0.0);
-    STATIC_REQUIRE(phi::is_nan(std::numeric_limits<double>::quiet_NaN()));
-    STATIC_REQUIRE(phi::is_nan(std::numeric_limits<double>::signaling_NaN()));
+    STATIC_TEST_IS_NAN(std::numeric_limits<double>::quiet_NaN());
+    STATIC_TEST_IS_NAN(std::numeric_limits<double>::signaling_NaN());
 
-    STATIC_REQUIRE_FALSE(phi::is_nan(0.0));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-0.0));
-    STATIC_REQUIRE_FALSE(phi::is_nan(1.0));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-1.0));
-    STATIC_REQUIRE_FALSE(phi::is_nan(HUGE_VAL));
-    STATIC_REQUIRE_FALSE(phi::is_nan(DBL_MIN / 2.0));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<double>::epsilon()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<double>::infinity()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-std::numeric_limits<double>::infinity()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<double>::min()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<double>::max()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<double>::lowest()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<double>::denorm_min()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<double>::round_error()));
+    STATIC_TEST_IS_NOT_NAN(0.0);
+    STATIC_TEST_IS_NOT_NAN(-0.0);
+    STATIC_TEST_IS_NOT_NAN(1.0);
+    STATIC_TEST_IS_NOT_NAN(-1.0);
+    STATIC_TEST_IS_NOT_NAN(HUGE_VAL);
+    STATIC_TEST_IS_NOT_NAN(DBL_MIN / 2.0);
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<double>::epsilon());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<double>::infinity());
+    STATIC_TEST_IS_NOT_NAN(-std::numeric_limits<double>::infinity());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<double>::min());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<double>::max());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<double>::lowest());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<double>::denorm_min());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<double>::round_error());
 
     // long double
     test_is_nan(std::nanl(""));
@@ -105,23 +117,23 @@ TEST_CASE("is_nan")
     test_is_nan(0.0L / 0.0L);
 #endif
     test_is_nan(std::numeric_limits<long double>::infinity() * 0.0L);
-    STATIC_REQUIRE(phi::is_nan(std::numeric_limits<long double>::quiet_NaN()));
-    STATIC_REQUIRE(phi::is_nan(std::numeric_limits<long double>::signaling_NaN()));
+    STATIC_TEST_IS_NAN(std::numeric_limits<long double>::quiet_NaN());
+    STATIC_TEST_IS_NAN(std::numeric_limits<long double>::signaling_NaN());
 
-    STATIC_REQUIRE_FALSE(phi::is_nan(0.0L));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-0.0L));
-    STATIC_REQUIRE_FALSE(phi::is_nan(1.0L));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-1.0L));
-    STATIC_REQUIRE_FALSE(phi::is_nan(HUGE_VALL));
-    STATIC_REQUIRE_FALSE(phi::is_nan(LDBL_MIN / 2.0L));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<long double>::epsilon()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<long double>::infinity()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(-std::numeric_limits<long double>::infinity()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<long double>::min()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<long double>::max()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<long double>::lowest()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<long double>::denorm_min()));
-    STATIC_REQUIRE_FALSE(phi::is_nan(std::numeric_limits<long double>::round_error()));
+    STATIC_TEST_IS_NOT_NAN(0.0L);
+    STATIC_TEST_IS_NOT_NAN(-0.0L);
+    STATIC_TEST_IS_NOT_NAN(1.0L);
+    STATIC_TEST_IS_NOT_NAN(-1.0L);
+    STATIC_TEST_IS_NOT_NAN(HUGE_VALL);
+    STATIC_TEST_IS_NOT_NAN(LDBL_MIN / 2.0L);
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<long double>::epsilon());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<long double>::infinity());
+    STATIC_TEST_IS_NOT_NAN(-std::numeric_limits<long double>::infinity());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<long double>::min());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<long double>::max());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<long double>::lowest());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<long double>::denorm_min());
+    STATIC_TEST_IS_NOT_NAN(std::numeric_limits<long double>::round_error());
 
     // floating_point<float>
     test_is_nan(sf(std::nanf("")));
@@ -135,25 +147,25 @@ TEST_CASE("is_nan")
     test_is_nan(sf(NAN));
     test_is_nan(sf(INFINITY) * sf(0.0f));
     test_is_nan(std::numeric_limits<sf>::infinity() * sf(0.0f));
-    STATIC_REQUIRE(phi::is_nan(sf(std::numeric_limits<float>::quiet_NaN())));
-    STATIC_REQUIRE(phi::is_nan(sf(std::numeric_limits<float>::signaling_NaN())));
+    STATIC_TEST_IS_NAN(sf(std::numeric_limits<float>::quiet_NaN()));
+    STATIC_TEST_IS_NAN(sf(std::numeric_limits<float>::signaling_NaN()));
 
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(0.0f)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(-0.0f)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(1.0f)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(-1.0f)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(INFINITY)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(-INFINITY)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(HUGE_VALF)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(FLT_MIN / 2.0f)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(std::numeric_limits<float>::epsilon())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(std::numeric_limits<float>::infinity())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(-std::numeric_limits<float>::infinity())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(std::numeric_limits<float>::min())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(std::numeric_limits<float>::max())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(std::numeric_limits<float>::lowest())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(std::numeric_limits<float>::denorm_min())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sf(std::numeric_limits<float>::round_error())));
+    STATIC_TEST_IS_NOT_NAN(sf(0.0f));
+    STATIC_TEST_IS_NOT_NAN(sf(-0.0f));
+    STATIC_TEST_IS_NOT_NAN(sf(1.0f));
+    STATIC_TEST_IS_NOT_NAN(sf(-1.0f));
+    STATIC_TEST_IS_NOT_NAN(sf(INFINITY));
+    STATIC_TEST_IS_NOT_NAN(sf(-INFINITY));
+    STATIC_TEST_IS_NOT_NAN(sf(HUGE_VALF));
+    STATIC_TEST_IS_NOT_NAN(sf(FLT_MIN / 2.0f));
+    STATIC_TEST_IS_NOT_NAN(sf(std::numeric_limits<float>::epsilon()));
+    STATIC_TEST_IS_NOT_NAN(sf(std::numeric_limits<float>::infinity()));
+    STATIC_TEST_IS_NOT_NAN(sf(-std::numeric_limits<float>::infinity()));
+    STATIC_TEST_IS_NOT_NAN(sf(std::numeric_limits<float>::min()));
+    STATIC_TEST_IS_NOT_NAN(sf(std::numeric_limits<float>::max()));
+    STATIC_TEST_IS_NOT_NAN(sf(std::numeric_limits<float>::lowest()));
+    STATIC_TEST_IS_NOT_NAN(sf(std::numeric_limits<float>::denorm_min()));
+    STATIC_TEST_IS_NOT_NAN(sf(std::numeric_limits<float>::round_error()));
 
     // floating_point<double>
     test_is_nan(sd(std::nan("")));
@@ -164,23 +176,23 @@ TEST_CASE("is_nan")
     test_is_nan(sd(0.0 / 0.0));
 #endif
     test_is_nan(std::numeric_limits<sd>::infinity() * sd(0.0));
-    STATIC_REQUIRE(phi::is_nan(sd(std::numeric_limits<double>::quiet_NaN())));
-    STATIC_REQUIRE(phi::is_nan(sd(std::numeric_limits<double>::signaling_NaN())));
+    STATIC_TEST_IS_NAN(sd(std::numeric_limits<double>::quiet_NaN()));
+    STATIC_TEST_IS_NAN(sd(std::numeric_limits<double>::signaling_NaN()));
 
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(0.0)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(-0.0)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(1.0)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(-1.0)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(HUGE_VAL)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(DBL_MIN / 2.0)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(std::numeric_limits<double>::epsilon())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(std::numeric_limits<double>::infinity())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(-std::numeric_limits<double>::infinity())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(std::numeric_limits<double>::min())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(std::numeric_limits<double>::max())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(std::numeric_limits<double>::lowest())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(std::numeric_limits<double>::denorm_min())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sd(std::numeric_limits<double>::round_error())));
+    STATIC_TEST_IS_NOT_NAN(sd(0.0));
+    STATIC_TEST_IS_NOT_NAN(sd(-0.0));
+    STATIC_TEST_IS_NOT_NAN(sd(1.0));
+    STATIC_TEST_IS_NOT_NAN(sd(-1.0));
+    STATIC_TEST_IS_NOT_NAN(sd(HUGE_VAL));
+    STATIC_TEST_IS_NOT_NAN(sd(DBL_MIN / 2.0));
+    STATIC_TEST_IS_NOT_NAN(sd(std::numeric_limits<double>::epsilon()));
+    STATIC_TEST_IS_NOT_NAN(sd(std::numeric_limits<double>::infinity()));
+    STATIC_TEST_IS_NOT_NAN(sd(-std::numeric_limits<double>::infinity()));
+    STATIC_TEST_IS_NOT_NAN(sd(std::numeric_limits<double>::min()));
+    STATIC_TEST_IS_NOT_NAN(sd(std::numeric_limits<double>::max()));
+    STATIC_TEST_IS_NOT_NAN(sd(std::numeric_limits<double>::lowest()));
+    STATIC_TEST_IS_NOT_NAN(sd(std::numeric_limits<double>::denorm_min()));
+    STATIC_TEST_IS_NOT_NAN(sd(std::numeric_limits<double>::round_error()));
 
     // floating_point<long double>
     test_is_nan(sl(std::nanl("")));
@@ -191,29 +203,41 @@ TEST_CASE("is_nan")
     test_is_nan(sl(0.0L / 0.0L));
 #endif
     test_is_nan(std::numeric_limits<sl>::infinity() * sl(0.0L));
-    STATIC_REQUIRE(phi::is_nan(sl(std::numeric_limits<long double>::quiet_NaN())));
-    STATIC_REQUIRE(phi::is_nan(sl(std::numeric_limits<long double>::signaling_NaN())));
+    STATIC_TEST_IS_NAN(sl(std::numeric_limits<long double>::quiet_NaN()));
+    STATIC_TEST_IS_NAN(sl(std::numeric_limits<long double>::signaling_NaN()));
 
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(0.0L)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(-0.0L)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(1.0L)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(-1.0L)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(HUGE_VALL)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(LDBL_MIN / 2.0L)));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(std::numeric_limits<long double>::epsilon())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(std::numeric_limits<long double>::infinity())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(-std::numeric_limits<long double>::infinity())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(std::numeric_limits<long double>::min())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(std::numeric_limits<long double>::max())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(std::numeric_limits<long double>::lowest())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(std::numeric_limits<long double>::denorm_min())));
-    STATIC_REQUIRE_FALSE(phi::is_nan(sl(std::numeric_limits<long double>::round_error())));
+    STATIC_TEST_IS_NOT_NAN(sl(0.0L));
+    STATIC_TEST_IS_NOT_NAN(sl(-0.0L));
+    STATIC_TEST_IS_NOT_NAN(sl(1.0L));
+    STATIC_TEST_IS_NOT_NAN(sl(-1.0L));
+    STATIC_TEST_IS_NOT_NAN(sl(HUGE_VALL));
+    STATIC_TEST_IS_NOT_NAN(sl(LDBL_MIN / 2.0L));
+    STATIC_TEST_IS_NOT_NAN(sl(std::numeric_limits<long double>::epsilon()));
+    STATIC_TEST_IS_NOT_NAN(sl(std::numeric_limits<long double>::infinity()));
+    STATIC_TEST_IS_NOT_NAN(sl(-std::numeric_limits<long double>::infinity()));
+    STATIC_TEST_IS_NOT_NAN(sl(std::numeric_limits<long double>::min()));
+    STATIC_TEST_IS_NOT_NAN(sl(std::numeric_limits<long double>::max()));
+    STATIC_TEST_IS_NOT_NAN(sl(std::numeric_limits<long double>::lowest()));
+    STATIC_TEST_IS_NOT_NAN(sl(std::numeric_limits<long double>::denorm_min()));
+    STATIC_TEST_IS_NOT_NAN(sl(std::numeric_limits<long double>::round_error()));
 
     // Noexcept
     CHECK_NOEXCEPT(phi::is_nan(std::nanf("")));
+    CHECK_NOEXCEPT(phi::is_nan(0.0f));
+    CHECK_NOEXCEPT(phi::is_nan(0.0f));
+
     CHECK_NOEXCEPT(phi::is_nan(std::nan("")));
+    CHECK_NOEXCEPT(phi::is_nan(0.0));
+
     CHECK_NOEXCEPT(phi::is_nan(std::nanl("")));
+    CHECK_NOEXCEPT(phi::is_nan(0.0L));
+
     CHECK_NOEXCEPT(phi::is_nan(sf(std::nanf(""))));
+    CHECK_NOEXCEPT(phi::is_nan(sf(0.0f)));
+
     CHECK_NOEXCEPT(phi::is_nan(sd(std::nan(""))));
+    CHECK_NOEXCEPT(phi::is_nan(sd(0.0)));
+
     CHECK_NOEXCEPT(phi::is_nan(sl(std::nanl(""))));
+    CHECK_NOEXCEPT(phi::is_nan(sl(0.0L)));
 }
