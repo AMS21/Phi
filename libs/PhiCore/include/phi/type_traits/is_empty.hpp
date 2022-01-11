abbba4908186404599debb35e5c082cc519771ec
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-01-11 15:44:00 +0100

    [type_traits] Add `is_not_empty`

--- libs/PhiCore/include/phi/type_traits/is_empty.hpp
+++ libs/PhiCore/include/phi/type_traits/is_empty.hpp
@@ -10,14 +10,17 @@
 #include "phi/compiler_support/inline_variables.hpp"
 #include "phi/compiler_support/intrinsics/is_empty.hpp"
 #include "phi/type_traits/integral_constant.hpp"
-#include "phi/type_traits/is_class.hpp"
-
-DETAIL_PHI_BEGIN_NAMESPACE()
 
 #if PHI_SUPPORTS_IS_EMPTY()
 
+DETAIL_PHI_BEGIN_NAMESPACE()
+
 template <typename TypeT>
 struct is_empty : public bool_constant<PHI_IS_EMPTY(TypeT)>
+{};
+
+template <typename TypeT>
+struct is_not_empty : public bool_constant<!PHI_IS_EMPTY(TypeT)>
 {};
 
 #    if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
@@ -25,9 +28,16 @@ struct is_empty : public bool_constant<PHI_IS_EMPTY(TypeT)>
 template <typename TypeT>
 PHI_INLINE_VARIABLE constexpr bool is_empty_v = PHI_IS_EMPTY(TypeT);
 
+template <typename TypeT>
+PHI_INLINE_VARIABLE constexpr bool is_not_empty_v = !PHI_IS_EMPTY(TypeT);
+
 #    endif
 
 #else
+
+#    include "phi/type_traits/is_class.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
 
 namespace detail
 {
@@ -42,7 +52,7 @@ namespace detail
         double lx;
     };
 
-    template <typename TypeT, bool = is_class_v<TypeT>>
+    template <typename TypeT, bool = is_class<TypeT>::value>
     struct is_empty_impl
         : public bool_constant<sizeof(is_empty_test1<TypeT>) == sizeof(is_empty_test2)>
     {};
@@ -56,10 +66,17 @@ template <typename TypeT>
 struct is_empty : public detail::is_empty_impl<TypeT>
 {};
 
+template <typename TypeT>
+struct is_not_empty : public bool_bool_constant<!is_empty<TypeT>::value>
+{};
+
 #    if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
 
 template <typename TypeT>
 PHI_INLINE_VARIABLE constexpr bool is_empty_v = is_empty<TypeT>::value;
+
+template <typename TypeT>
+PHI_INLINE_VARIABLE constexpr bool is_not_empty_v = is_not_empty<TypeT>::value;
 
 #    endif
 
