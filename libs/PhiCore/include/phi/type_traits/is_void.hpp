ebbcc67bfef156b004cf86d7974d10ce1f3a2888
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-01-12 23:39:15 +0100

    [type_traits] Simplify implementation of is_void

--- libs/PhiCore/include/phi/type_traits/is_void.hpp
+++ libs/PhiCore/include/phi/type_traits/is_void.hpp
@@ -7,15 +7,13 @@
 #    pragma once
 #endif
 
-#include "phi/compiler_support/compiler.hpp"
 #include "phi/compiler_support/inline_variables.hpp"
 #include "phi/compiler_support/intrinsics/is_void.hpp"
-
-#if PHI_SUPPORTS_IS_VOID()
-
-#    include "phi/type_traits/integral_constant.hpp"
+#include "phi/type_traits/integral_constant.hpp"
 
 DETAIL_PHI_BEGIN_NAMESPACE()
+
+#if PHI_SUPPORTS_IS_VOID()
 
 template <typename TypeT>
 struct is_void : public bool_constant<PHI_IS_VOID(TypeT)>
@@ -37,17 +35,28 @@ PHI_INLINE_VARIABLE constexpr bool is_not_void_v = !PHI_IS_VOID(TypeT);
 
 #else
 
-#    include "phi/type_traits/is_same.hpp"
-#    include "phi/type_traits/remove_cv.hpp"
-
-DETAIL_PHI_BEGIN_NAMESPACE()
-
 template <typename TypeT>
-struct is_void : public is_same<void, typename remove_cv<TypeT>::type>
+struct is_void : public false_type
+{};
+
+template <>
+struct is_void<void> : public true_type
+{};
+
+template <>
+struct is_void<const void> : public true_type
+{};
+
+template <>
+struct is_void<volatile void> : public true_type
+{};
+
+template <>
+struct is_void<const volatile void> : public true_type
 {};
 
 template <typename TypeT>
-struct is_not_void : public is_not_same<void, typename remove_cv<TypeT>::type>
+struct is_not_void : public bool_constant<!is_void<TypeT>::value>
 {};
 
 #    if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
