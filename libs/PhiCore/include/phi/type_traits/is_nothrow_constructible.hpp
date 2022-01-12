002480b2452a16ab9e030fd0e62767df642619c8
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-01-12 18:53:18 +0100

    [type_traits] Add `is_not_nothrow_constructible´

--- libs/PhiCore/include/phi/type_traits/is_nothrow_constructible.hpp
+++ libs/PhiCore/include/phi/type_traits/is_nothrow_constructible.hpp
@@ -9,19 +9,20 @@
 
 #include "phi/compiler_support/inline_variables.hpp"
 #include "phi/compiler_support/intrinsics/is_nothrow_constructible.hpp"
-#include "phi/core/declval.hpp"
-#include "phi/core/size_t.hpp"
 #include "phi/type_traits/integral_constant.hpp"
-#include "phi/type_traits/is_constructible.hpp"
-#include "phi/type_traits/is_reference.hpp"
-
-DETAIL_PHI_BEGIN_NAMESPACE()
 
 #if PHI_SUPPORTS_IS_NOTHROW_CONSTRUCTIBLE()
+
+DETAIL_PHI_BEGIN_NAMESPACE()
 
 template <typename TypeT, typename... ArgsT>
 struct is_nothrow_constructible
     : public bool_constant<PHI_IS_NOTHROW_CONSTRUCTIBLE(TypeT, ArgsT...)>
+{};
+
+template <typename TypeT, typename... ArgsT>
+struct is_not_nothrow_constructible
+    : public bool_constant<!PHI_IS_NOTHROW_CONSTRUCTIBLE(TypeT, ArgsT...)>
 {};
 
 #    if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
@@ -30,9 +31,20 @@ template <typename TypeT, typename... ArgsT>
 PHI_INLINE_VARIABLE constexpr bool is_nothrow_constructible_v =
         PHI_IS_NOTHROW_CONSTRUCTIBLE(TypeT, ArgsT...);
 
+template <typename TypeT, typename... ArgsT>
+PHI_INLINE_VARIABLE constexpr bool is_not_nothrow_constructible_v =
+        !PHI_IS_NOTHROW_CONSTRUCTIBLE(TypeT, ArgsT...);
+
 #    endif
 
 #else
+
+#    include "phi/core/declval.hpp"
+#    include "phi/core/size_t.hpp"
+#    include "phi/type_traits/is_constructible.hpp"
+#    include "phi/type_traits/is_reference.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
 
 namespace detail
 {
@@ -63,14 +75,19 @@ namespace detail
 
 template <typename TypeT, typename... ArgsT>
 struct is_nothrow_constructible
-    : detail::is_nothrow_constructible_impl<is_constructible<TypeT, ArgsT...>::value,
-                                            is_reference<TypeT>::value, TypeT, ArgsT...>
+    : public detail::is_nothrow_constructible_impl<is_constructible<TypeT, ArgsT...>::value,
+                                                   is_reference<TypeT>::value, TypeT, ArgsT...>
 {};
 
 template <typename TypeT, size_t Dimension>
 struct is_nothrow_constructible<TypeT[Dimension]>
-    : detail::is_nothrow_constructible_impl<is_constructible<TypeT>::value,
-                                            is_reference<TypeT>::value, TypeT>
+    : public detail::is_nothrow_constructible_impl<is_constructible<TypeT>::value,
+                                                   is_reference<TypeT>::value, TypeT>
+{};
+
+template <typename TypeT, typename... ArgsT>
+struct is_not_nothrow_constructible
+    : public bool_constant<!is_nothrow_constructible<TypeT, ArgsT...>::value>
 {};
 
 #    if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
@@ -78,6 +95,10 @@ struct is_nothrow_constructible<TypeT[Dimension]>
 template <typename TypeT, typename... ArgsT>
 PHI_INLINE_VARIABLE constexpr bool is_nothrow_constructible_v =
         is_nothrow_constructible<TypeT, ArgsT...>::value;
+
+template <typename TypeT, typename... ArgsT>
+PHI_INLINE_VARIABLE constexpr bool is_not_nothrow_constructible_v =
+        is_not_nothrow_constructible<TypeT, ArgsT...>::value;
 
 #    endif
 
