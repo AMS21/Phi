2b537bb476e0152083edbeaa710bbc91c7c12d40
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-01-12 19:28:34 +0100

    [type_traits] Add `is_not_nothrow_copy_assignable`

--- libs/PhiCore/include/phi/type_traits/is_nothrow_copy_assignable.hpp
+++ libs/PhiCore/include/phi/type_traits/is_nothrow_copy_assignable.hpp
@@ -9,10 +9,9 @@
 
 #include "phi/compiler_support/inline_variables.hpp"
 #include "phi/compiler_support/intrinsics/is_nothrow_copy_assignable.hpp"
+#include "phi/type_traits/integral_constant.hpp"
 
 #if PHI_SUPPORTS_IS_NOTHROW_COPY_ASSIGNABLE()
-
-#    include "phi/type_traits/integral_constant.hpp"
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
@@ -20,11 +19,19 @@ template <typename TypeT>
 struct is_nothrow_copy_assignable : public bool_constant<PHI_IS_NOTHROW_COPY_ASSIGNABLE(TypeT)>
 {};
 
+template <typename TypeT>
+struct is_not_nothrow_copy_assignable : public bool_constant<!PHI_IS_NOTHROW_COPY_ASSIGNABLE(TypeT)>
+{};
+
 #    if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
 
 template <typename TypeT>
 PHI_INLINE_VARIABLE constexpr bool is_nothrow_copy_assignable_v =
         PHI_IS_NOTHROW_COPY_ASSIGNABLE(TypeT);
+
+template <typename TypeT>
+PHI_INLINE_VARIABLE constexpr bool is_not_nothrow_copy_assignable_v =
+        !PHI_IS_NOTHROW_COPY_ASSIGNABLE(TypeT);
 
 #    endif
 #else
@@ -37,8 +44,14 @@ DETAIL_PHI_BEGIN_NAMESPACE()
 
 template <typename TypeT>
 struct is_nothrow_copy_assignable
-    : public is_nothrow_assignable<add_lvalue_reference_t<TypeT>,
-                                   add_lvalue_reference_t<add_const_t<TypeT>>>
+    : public is_nothrow_assignable<
+              typename add_lvalue_reference<TypeT>::type,
+              typename add_lvalue_reference<typename add_const<TypeT>::type>::type>
+{};
+
+template <typename TypeT>
+struct is_not_nothrow_copy_assignable
+    : public bool_constant<!is_nothrow_copy_assignable<TypeT>::value>
 {};
 
 #    if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
@@ -46,6 +59,10 @@ struct is_nothrow_copy_assignable
 template <typename TypeT>
 PHI_INLINE_VARIABLE constexpr bool is_nothrow_copy_assignable_v =
         is_nothrow_copy_assignable<TypeT>::value;
+
+template <typename TypeT>
+PHI_INLINE_VARIABLE constexpr bool is_not_nothrow_copy_assignable_v =
+        is_not_nothrow_copy_assignable<TypeT>::value;
 
 #    endif
 
