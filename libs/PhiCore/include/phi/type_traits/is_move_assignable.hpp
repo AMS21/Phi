05a4ae5f030bf5d372a6a35f7f39885983f57605
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-01-12 16:23:29 +0100

    [type_traits] Add is_not_move_assignable

--- libs/PhiCore/include/phi/type_traits/is_move_assignable.hpp
+++ libs/PhiCore/include/phi/type_traits/is_move_assignable.hpp
@@ -10,19 +10,27 @@
 #include "phi/compiler_support/inline_variables.hpp"
 #include "phi/type_traits/add_lvalue_reference.hpp"
 #include "phi/type_traits/add_rvalue_reference.hpp"
+#include "phi/type_traits/integral_constant.hpp"
 #include "phi/type_traits/is_assignable.hpp"
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
 template <typename TypeT>
-struct is_move_assignable
-    : public is_assignable<add_lvalue_reference_t<TypeT>, add_rvalue_reference_t<TypeT>>
+struct is_move_assignable : public is_assignable<typename add_lvalue_reference<TypeT>::type,
+                                                 typename add_rvalue_reference<TypeT>::type>
+{};
+
+template <typename TypeT>
+struct is_not_move_assignable : public bool_constant<!is_move_assignable<TypeT>::value>
 {};
 
 #if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
 
 template <typename TypeT>
 PHI_INLINE_VARIABLE constexpr bool is_move_assignable_v = is_move_assignable<TypeT>::value;
+
+template <typename TypeT>
+PHI_INLINE_VARIABLE constexpr bool is_not_move_assignable_v = is_not_move_assignable<TypeT>::value;
 
 #endif
 
