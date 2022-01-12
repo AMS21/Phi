59557badbcccc922c063fce0e466f9ecc42f7dbf
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-01-12 16:43:55 +0100

    [type_traits] Add `is_not_move_constructible`

--- libs/PhiCore/include/phi/type_traits/is_move_constructible.hpp
+++ libs/PhiCore/include/phi/type_traits/is_move_constructible.hpp
@@ -9,18 +9,28 @@
 
 #include "phi/compiler_support/inline_variables.hpp"
 #include "phi/type_traits/add_rvalue_reference.hpp"
+#include "phi/type_traits/integral_constant.hpp"
 #include "phi/type_traits/is_constructible.hpp"
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
 template <typename TypeT>
-struct is_move_constructible : public is_constructible<TypeT, add_rvalue_reference_t<TypeT>>
+struct is_move_constructible
+    : public is_constructible<TypeT, typename add_rvalue_reference<TypeT>::type>
+{};
+
+template <typename TypeT>
+struct is_not_move_constructible : public bool_constant<!is_move_constructible<TypeT>::value>
 {};
 
 #if PHI_HAS_FEATURE_VARIABLE_TEMPLATE()
 
 template <typename TypeT>
 PHI_INLINE_VARIABLE constexpr bool is_move_constructible_v = is_move_constructible<TypeT>::value;
+
+template <typename TypeT>
+PHI_INLINE_VARIABLE constexpr bool is_not_move_constructible_v =
+        is_not_move_constructible<TypeT>::value;
 
 #endif
 
