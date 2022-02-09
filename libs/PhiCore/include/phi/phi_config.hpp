5438d1ff6e2c4747b443432b05242a3916ff01c9
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-02-09 15:24:50 +0100

    [Misc] Silence warning about used and unused

--- libs/PhiCore/include/phi/phi_config.hpp
+++ libs/PhiCore/include/phi/phi_config.hpp
@@ -1,6 +1,7 @@
 #ifndef INCG_PHI_PHICONFIG_HPP
 #define INCG_PHI_PHICONFIG_HPP
 
+#include "phi/compiler_support/warning.hpp"
 #include "phi/generated/compiler_support/features.hpp"
 
 #if PHI_HAS_EXTENSION_PRAGMA_ONCE()
@@ -9,7 +10,6 @@
 
 #include "phi/compiler_support/compiler.hpp"
 #include "phi/compiler_support/platform.hpp"
-#include "phi/compiler_support/unused.hpp"
 #include "phi/preprocessor/glue.hpp"
 #include "phi/preprocessor/stringify.hpp"
 #include "phi/preprocessor/versioning.hpp"
@@ -75,15 +75,19 @@
 #    define PHI_API
 #endif
 
+PHI_GCC_SUPPRESS_WARNING_PUSH()
+PHI_GCC_SUPPRESS_WARNING("-Wunused-const-variable")
+
 DETAIL_PHI_BEGIN_NAMESPACE()
 
-PHI_UNUSED constexpr const static std::uint32_t VersionMajor{PHI_VERSION_MAJOR()};
-PHI_UNUSED constexpr const static std::uint32_t VersionMinor{PHI_VERSION_MINOR()};
-PHI_UNUSED constexpr const static std::uint32_t VersionPatch{PHI_VERSION_PATCH()};
-PHI_UNUSED constexpr const static std::uint32_t Version{
-        PHI_VERSION()}; // NOLINT(hicpp-signed-bitwise)
-PHI_UNUSED constexpr const static char VersionString[]{PHI_VERSION_STR()};
+constexpr const static std::uint32_t VersionMajor{PHI_VERSION_MAJOR()};
+constexpr const static std::uint32_t VersionMinor{PHI_VERSION_MINOR()};
+constexpr const static std::uint32_t VersionPatch{PHI_VERSION_PATCH()};
+constexpr const static std::uint32_t Version{PHI_VERSION()}; // NOLINT(hicpp-signed-bitwise)
+constexpr const static char          VersionString[]{PHI_VERSION_STR()};
 
 DETAIL_PHI_END_NAMESPACE()
+
+PHI_GCC_SUPPRESS_WARNING_POP()
 
 #endif // INCG_PHI_PHICONFIG_HPP
