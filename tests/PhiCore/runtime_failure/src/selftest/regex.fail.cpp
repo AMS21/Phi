7d5f6aafc350f276f965cbf4451b1dfa1297f7f6
AMS21 <19614997+AMS21@users.noreply.github.com>
2022-03-03 19:20:32 +0100

    [Misc] Silence warnings form <iostream>

--- tests/PhiCore/runtime_failure/src/selftest/regex.fail.cpp
+++ tests/PhiCore/runtime_failure/src/selftest/regex.fail.cpp
@@ -1,4 +1,8 @@
+#include <phi/compiler_support/warning.hpp>
+
+PHI_EXTERNAL_HEADERS_BEGIN()
 #include <iostream>
+PHI_EXTERNAL_HEADERS_END()
 
 int main()
 {
--- tests/PhiTest/runtime_failure/src/Check.fail.cpp
+++ tests/PhiTest/runtime_failure/src/Check.fail.cpp
@@ -1,6 +1,10 @@
 #include <phi/test/test_macros.hpp>
 
+#include <phi/compiler_support/warning.hpp>
+
+PHI_EXTERNAL_HEADERS_BEGIN()
 #include <iostream>
+PHI_EXTERNAL_HEADERS_END()
 
 TEST_CASE("CHECK")
 {
