8626c05db3310a2502e376668bb632bf80e1df9b
AMS21 <AMS21.github@gmail.com>
2022-04-01 12:28:36 +0200

    [Text] Add `is_digit`

---
+++ tests/PhiCore/unittests/src/text/is_digit.test.cpp
@@ -0,0 +1,36 @@
+#include "phi/test/test_macros.hpp"
+
+#include "phi/text/is_digit.hpp"
+#include <cctype>
+
+void test_is_digit(const char c) noexcept
+{
+    CHECK(phi::is_digit(c));
+    CHECK(std::isdigit(c));
+}
+
+void test_is_not_digit(const char c) noexcept
+{
+    CHECK_FALSE(phi::is_digit(c));
+    CHECK_FALSE(std::isdigit(c));
+}
+
+TEST_CASE("is_digit")
+{
+    for (char c{0}; c <= 47; ++c)
+    {
+        test_is_not_digit(c);
+    }
+
+    for (char c{48}; c <= 57; ++c)
+    {
+        test_is_digit(c);
+    }
+
+    for (char c{58}; c < 127; ++c)
+    {
+        test_is_not_digit(c);
+    }
+
+    test_is_not_digit(127);
+}
