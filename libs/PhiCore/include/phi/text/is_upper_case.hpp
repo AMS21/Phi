c9b0d39f4fe7d0a37b921655090746c72fc68cfe
AMS21 <AMS21.github@gmail.com>
2022-04-01 12:07:57 +0200

    [Text] Add `is_upper_case`

---
+++ libs/PhiCore/include/phi/text/is_upper_case.hpp
@@ -0,0 +1,22 @@
+#ifndef INCG_PHI_CORE_TEXT_IS_UPPER_CASE_HPP
+#define INCG_PHI_CORE_TEXT_IS_UPPER_CASE_HPP
+
+#include "phi/phi_config.hpp"
+
+#if PHI_HAS_EXTENSION_PRAGMA_ONCE()
+#    pragma once
+#endif
+
+#include "phi/compiler_support/nodiscard.hpp"
+#include "phi/core/boolean.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+PHI_NODISCARD constexpr phi::boolean is_upper_case(const char c) noexcept
+{
+    return (c >= 'A' && c <= 'Z');
+}
+
+DETAIL_PHI_END_NAMESPACE()
+
+#endif // INCG_PHI_CORE_TEXT_IS_UPPER_CASE_HPP
---
+++ tests/PhiCore/unittests/src/text/is_upper_case.test.cpp
@@ -0,0 +1,36 @@
+#include "phi/test/test_macros.hpp"
+
+#include "phi/text/is_upper_case.hpp"
+#include <cctype>
+
+void test_is_upper_case(const char c) noexcept
+{
+    CHECK(phi::is_upper_case(c));
+    CHECK(std::isupper(c));
+}
+
+void test_is_not_upper_case(const char c) noexcept
+{
+    CHECK_FALSE(phi::is_upper_case(c));
+    CHECK_FALSE(std::isupper(c));
+}
+
+TEST_CASE("is_upper_case")
+{
+    for (char c{0}; c <= 64; ++c)
+    {
+        test_is_not_upper_case(c);
+    }
+
+    for (char c{65}; c <= 90; ++c)
+    {
+        test_is_upper_case(c);
+    }
+
+    for (char c{91}; c < 127; ++c)
+    {
+        test_is_not_upper_case(c);
+    }
+
+    test_is_not_upper_case(127);
+}
