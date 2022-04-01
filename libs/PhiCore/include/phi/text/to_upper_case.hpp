d32feeb8c03f89d22da738736ad51a6320228efd
AMS21 <AMS21.github@gmail.com>
2022-04-01 22:45:55 +0200

    [Text] Add `to_upper_case`

---
+++ libs/PhiCore/include/phi/text/to_upper_case.hpp
@@ -0,0 +1,24 @@
+#ifndef INCG_PHI_CORE_TEXT_TO_UPPER_CASE_HPP
+#define INCG_PHI_CORE_TEXT_TO_UPPER_CASE_HPP
+
+#include "phi/phi_config.hpp"
+
+#if PHI_HAS_EXTENSION_PRAGMA_ONCE()
+#    pragma once
+#endif
+
+#include "phi/compiler_support/nodiscard.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+// Implementation comparision: https://godbolt.org/z/1bEsG5nrE
+
+PHI_NODISCARD constexpr char to_upper_case(const char c) noexcept
+{
+    // NOLINTNEXTLINE(bugprone-narrowing-conversions,readability-magic-numbers,cppcoreguidelines-narrowing-conversions)
+    return (c >= 'a' && c <= 'z') ? static_cast<char>(c - 32) : c;
+}
+
+DETAIL_PHI_END_NAMESPACE()
+
+#endif // INCG_PHI_CORE_TEXT_TO_UPPER_CASE_HPP
---
+++ tests/PhiCore/unittests/src/text/to_upper_case.test.cpp
@@ -0,0 +1,30 @@
+#include "phi/test/test_macros.hpp"
+
+#include "phi/text/to_upper_case.hpp"
+#include <cctype>
+
+void test_to_upper_case(char original, char expected)
+{
+    CHECK(phi::to_upper_case(original) == expected);
+    CHECK(std::toupper(original) == expected);
+}
+
+TEST_CASE("to_upper_case")
+{
+    for (char c{0}; c <= 96; ++c)
+    {
+        test_to_upper_case(c, c);
+    }
+
+    for (char c{97}; c <= 122; ++c)
+    {
+        test_to_upper_case(c, static_cast<char>(c - 32));
+    }
+
+    for (char c{123}; c <= 126; ++c)
+    {
+        test_to_upper_case(c, c);
+    }
+
+    test_to_upper_case(127, 127);
+}
