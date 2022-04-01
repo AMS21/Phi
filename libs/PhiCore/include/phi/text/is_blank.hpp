a2dcc9120050ede97dbb7c1637318b4a76572d40
AMS21 <AMS21.github@gmail.com>
2022-04-01 16:20:13 +0200

    [Text] Add `is_blank`

---
+++ libs/PhiCore/include/phi/text/is_blank.hpp
@@ -0,0 +1,23 @@
+#ifndef INCG_PHI_CORE_TEXT_IS_BLANK_HPP
+#define INCG_PHI_CORE_TEXT_IS_BLANK_HPP
+
+#include "phi/phi_config.hpp"
+
+#if PHI_HAS_EXTENSION_PRAGMA_ONCE()
+#    pragma once
+#endif
+
+#include "phi/compiler_support/nodiscard.hpp"
+#include "phi/core/boolean.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+PHI_NODISCARD constexpr phi::boolean is_blank(const char c) noexcept
+{
+    // NOLINTNEXTLINE(readability-implicit-bool-conversion)
+    return static_cast<bool>(c == '\t' | c == ' ');
+}
+
+DETAIL_PHI_END_NAMESPACE()
+
+#endif // INCG_PHI_CORE_TEXT_IS_BLANK_HPP
---
+++ tests/PhiCore/unittests/src/text/is_blank.test.cpp
@@ -0,0 +1,40 @@
+#include "phi/test/test_macros.hpp"
+
+#include "phi/text/is_blank.hpp"
+#include <cctype>
+
+void test_is_blank(const char c) noexcept
+{
+    CHECK(phi::is_blank(c));
+    CHECK(std::isblank(c));
+}
+
+void test_is_not_blank(const char c) noexcept
+{
+    CHECK_FALSE(phi::is_blank(c));
+    CHECK_FALSE(std::isblank(c));
+}
+
+TEST_CASE("is_blank")
+{
+    for (char c{0}; c <= 8; ++c)
+    {
+        test_is_not_blank(c);
+    }
+
+    test_is_blank('\t');
+
+    for (char c{10}; c <= 31; ++c)
+    {
+        test_is_not_blank(c);
+    }
+
+    test_is_blank(' ');
+
+    for (char c{33}; c <= 126; ++c)
+    {
+        test_is_not_blank(c);
+    }
+
+    test_is_not_blank(127);
+}
