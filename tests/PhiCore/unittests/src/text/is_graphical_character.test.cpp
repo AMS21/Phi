8a35424c61c73b6261fa08b413c10ef27e8659a7
AMS21 <AMS21.github@gmail.com>
2022-04-01 16:04:44 +0200

    [Text] Implement `is_graphical_character`

---
+++ tests/PhiCore/unittests/src/text/is_graphical_character.test.cpp
@@ -0,0 +1,31 @@
+#include "phi/test/test_macros.hpp"
+
+#include "phi/text/is_graphical_character.hpp"
+#include <cctype>
+
+void test_is_graphical_character(const char c) noexcept
+{
+    CHECK(phi::is_graphical_character(c));
+    CHECK(std::isgraph(c));
+}
+
+void test_is_not_graphical_character(const char c) noexcept
+{
+    CHECK_FALSE(phi::is_graphical_character(c));
+    CHECK_FALSE(std::isgraph(c));
+}
+
+TEST_CASE("is_graphical_character")
+{
+    for (char c{0}; c <= 32; ++c)
+    {
+        test_is_not_graphical_character(c);
+    }
+
+    for (char c{33}; c <= 126; ++c)
+    {
+        test_is_graphical_character(c);
+    }
+
+    test_is_not_graphical_character(127);
+}
