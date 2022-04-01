3890a84215e0d5ae256e3673b97736ab1ec9413d
AMS21 <AMS21.github@gmail.com>
2022-04-01 16:24:08 +0200

    [Text] Add `is_printable`

---
+++ libs/PhiCore/include/phi/text/is_printable.hpp
@@ -0,0 +1,23 @@
+#ifndef INCG_PHI_CORE_TEXT_IS_PRINTABLE_HPP
+#define INCG_PHI_CORE_TEXT_IS_PRINTABLE_HPP
+
+#include "phi/phi_config.hpp"
+
+#if PHI_HAS_EXTENSION_PRAGMA_ONCE()
+#    pragma once
+#endif
+
+#include "phi/compiler_support/nodiscard.hpp"
+#include "phi/core/boolean.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+PHI_NODISCARD constexpr phi::boolean is_printable(const char c) noexcept
+{
+    // NOLINTNEXTLINE(readability-magic-numbers)
+    return (c >= 32 && c <= 126);
+}
+
+DETAIL_PHI_END_NAMESPACE()
+
+#endif // INCG_PHI_CORE_TEXT_IS_PRINTABLE_HPP
---
+++ tests/PhiCore/unittests/src/text/is_printable.test.cpp
@@ -0,0 +1,31 @@
+#include "phi/test/test_macros.hpp"
+
+#include "phi/text/is_printable.hpp"
+#include <cctype>
+
+void test_is_printable(const char c) noexcept
+{
+    CHECK(phi::is_printable(c));
+    CHECK(std::isprint(c));
+}
+
+void test_is_not_printable(const char c) noexcept
+{
+    CHECK_FALSE(phi::is_printable(c));
+    CHECK_FALSE(std::isprint(c));
+}
+
+TEST_CASE("is_printable")
+{
+    for (char c{0}; c <= 31; ++c)
+    {
+        test_is_not_printable(c);
+    }
+
+    for (char c{32}; c <= 126; ++c)
+    {
+        test_is_printable(c);
+    }
+
+    test_is_not_printable(127);
+}
