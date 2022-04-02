52c30fda6cf1bbdeca49f1f849d80177e492861a
AMS21 <AMS21.github@gmail.com>
2022-04-02 15:21:37 +0200

    Some minor stuff to SetupEnvironment

--- scripts/ci/SetupEnvironment.sh
+++ scripts/ci/SetupEnvironment.sh
@@ -2,13 +2,21 @@
 
 # Get number of processors available
 echo "PROC_COUNT=$(nproc)" >>$GITHUB_ENV
-echo "OS_CODENAME=$(lsb_release -c -s)" >> $GITHUB_ENV
+echo "OS_CODENAME=$(lsb_release -c -s)" >>$GITHUB_ENV
 
 # Setup CMake stuff
+# https://cmake.org/cmake/help/latest/envvar/CMAKE_BUILD_PARALLEL_LEVEL.html
 echo "CMAKE_BUILD_PARALLEL_LEVEL=${PROC_COUNT}" >>$GITHUB_ENV
+# https://cmake.org/cmake/help/latest/envvar/CTEST_PARALLEL_LEVEL.html
 echo "CTEST_PARALLEL_LEVEL=${PROC_COUNT}" >>$GITHUB_ENV
+# https://cmake.org/cmake/help/latest/envvar/CTEST_OUTPUT_ON_FAILURE.html
 echo "CTEST_OUTPUT_ON_FAILURE=ON" >>$GITHUB_ENV
-echo "CMAKE_DEFAULT_CONFIGURE_OPTIONS=-DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON" >>$GITHUB_ENV
+
+# Set some default options
+# https://cmake.org/cmake/help/latest/envvar/CMAKE_EXPORT_COMPILE_COMMANDS.html
+echo "CMAKE_EXPORT_COMPILE_COMMANDS=ON"
+# https://cmake.org/cmake/help/latest/envvar/VERBOSE.html
+echo "VERBOSE=ON" >>$GITHUB_ENV
 
 # Latest versions
 echo "LATEST_LLVM_VERSION=14" >>$GITHUB_ENV
