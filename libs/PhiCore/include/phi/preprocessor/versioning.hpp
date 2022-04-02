092d10f9daab159726066f5a310d145aa39144ac
AMS21 <AMS21.github@gmail.com>
2022-04-02 18:21:48 +0200

    [Misc] Silence warnings about signed-bitwise

--- libs/PhiCore/include/phi/preprocessor/versioning.hpp
+++ libs/PhiCore/include/phi/preprocessor/versioning.hpp
@@ -1,11 +1,13 @@
 #ifndef INCG_PHI_CORE_PREPROCESSOR_VERSIONING_HPP
 #define INCG_PHI_CORE_PREPROCESSOR_VERSIONING_HPP
 
-#define PHI_VERSION_CREATE(major, minor, patch) (((major) << 24) + ((minor) << 16) + (patch))
+#define PHI_VERSION_CREATE(major, minor, patch)                                                    \
+    (((major) << 24) + ((minor) << 16) + (patch)) /* NOLINT(hicpp-signed-bitwise) */
 
-#define PHI_VERSION_EXTRACT_MAJOR(version) ((version) >> 24)
+#define PHI_VERSION_EXTRACT_MAJOR(version) ((version) >> 24) /* NOLINT(hicpp-signed-bitwise) */
 
-#define PHI_VERSION_EXTRACT_MINOR(version) (((version) >> 16) % 256)
+#define PHI_VERSION_EXTRACT_MINOR(version)                                                         \
+    (((version) >> 16) % 256) /* NOLINT(hicpp-signed-bitwise) */
 
 #define PHI_VERSION_EXTRACT_PATCH(version) ((version) % 256)
 
