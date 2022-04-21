cba6210eed090e27c514c2cf074de7f282018d23
AMS21 <AMS21.github@gmail.com>
2022-04-21 23:01:01 +0200

    [Tests] Improve tests for `copy_x` type traits

--- tests/PhiCore/unittests/src/type_traits/copy_const.test.cpp
+++ tests/PhiCore/unittests/src/type_traits/copy_const.test.cpp
@@ -2,39 +2,50 @@
 
 #include <phi/type_traits/copy_const.hpp>
 
+template <typename T, typename U, typename V>
+void test_copy_const_impl()
+{
+    CHECK_SAME_TYPE(typename phi::copy_const<T, U>::type, V);
+    CHECK_SAME_TYPE(phi::copy_const_t<T, U>, V);
+}
+
+template <typename T, typename U>
+void test_copy_const()
+{
+    test_copy_const_impl<T, U, U>();
+    test_copy_const_impl<T, const U, const U>();
+    test_copy_const_impl<T, volatile U, volatile U>();
+    test_copy_const_impl<T, const volatile U, const volatile U>();
+
+    test_copy_const_impl<const T, U, const U>();
+    test_copy_const_impl<const T, const U, const U>();
+    test_copy_const_impl<const T, volatile U, const volatile U>();
+    test_copy_const_impl<const T, const volatile U, const volatile U>();
+
+    test_copy_const_impl<volatile T, U, U>();
+    test_copy_const_impl<volatile T, const U, const U>();
+    test_copy_const_impl<volatile T, volatile U, volatile U>();
+    test_copy_const_impl<volatile T, const volatile U, const volatile U>();
+
+    test_copy_const_impl<const volatile T, U, const U>();
+    test_copy_const_impl<const volatile T, const U, const U>();
+    test_copy_const_impl<const volatile T, volatile U, const volatile U>();
+    test_copy_const_impl<const volatile T, const volatile U, const volatile U>();
+}
+
+struct A
+{};
+
 TEST_CASE("copy_const")
 {
-    CHECK_SAME_TYPE(typename phi::copy_const<int, float>::type, float);
-    CHECK_SAME_TYPE(typename phi::copy_const<volatile int, float>::type, float);
-    CHECK_SAME_TYPE(typename phi::copy_const<int, volatile float>::type, volatile float);
-    CHECK_SAME_TYPE(typename phi::copy_const<volatile int, volatile float>::type, volatile float);
-
-    CHECK_SAME_TYPE(typename phi::copy_const<int, const float>::type, const float);
-    CHECK_SAME_TYPE(typename phi::copy_const<const int, float>::type, const float);
-    CHECK_SAME_TYPE(typename phi::copy_const<const int, const float>::type, const float);
-
-    CHECK_SAME_TYPE(typename phi::copy_const<const volatile int, float>::type, const float);
-    CHECK_SAME_TYPE(typename phi::copy_const<int, const volatile float>::type,
-                    const volatile float);
-    CHECK_SAME_TYPE(typename phi::copy_const<const volatile int, const volatile float>::type,
-                    const volatile float);
-
-    CHECK_SAME_TYPE(typename phi::copy_const<const int, volatile float>::type,
-                    const volatile float);
-
-    CHECK_SAME_TYPE(typename phi::copy_const_t<int, float>, float);
-    CHECK_SAME_TYPE(typename phi::copy_const_t<volatile int, float>, float);
-    CHECK_SAME_TYPE(typename phi::copy_const_t<int, volatile float>, volatile float);
-    CHECK_SAME_TYPE(typename phi::copy_const_t<volatile int, volatile float>, volatile float);
-
-    CHECK_SAME_TYPE(typename phi::copy_const_t<int, const float>, const float);
-    CHECK_SAME_TYPE(typename phi::copy_const_t<const int, float>, const float);
-    CHECK_SAME_TYPE(typename phi::copy_const_t<const int, const float>, const float);
-
-    CHECK_SAME_TYPE(typename phi::copy_const_t<const volatile int, float>, const float);
-    CHECK_SAME_TYPE(typename phi::copy_const_t<int, const volatile float>, const volatile float);
-    CHECK_SAME_TYPE(typename phi::copy_const_t<const volatile int, const volatile float>,
-                    const volatile float);
-
-    CHECK_SAME_TYPE(typename phi::copy_const_t<const int, volatile float>, const volatile float);
+    test_copy_const<void, void>();
+    test_copy_const<void, int>();
+    test_copy_const<int, void>();
+    test_copy_const<int, int>();
+    test_copy_const<float, float>();
+    test_copy_const<int, float>();
+    test_copy_const<float, int>();
+    test_copy_const<A, A>();
+    test_copy_const<A, int>();
+    test_copy_const<int, A>();
 }
