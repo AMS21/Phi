9337ef6ff0d2670c8e14ec157542fd8e189127e1
AMS21 <AMS21.github@gmail.com>
2022-04-21 22:34:39 +0200

    [CI+Scripts]: Add automatic link archiving

---
+++ scripts/ci/archive_all_links.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Regex for html
+# Copied from here https://stackoverflow.com/a/3184819/10114628
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+
+echo "Scanning all files in \"$(pwd)\"..."
+
+all_files=$(find . -type f ! -path "./.git/*" -exec grep -Iq . {} \; -print)
+
+# 3 Request per minute
+sleep_time=20s
+
+# Iterate over each file
+for file in $all_files; do
+    echo "Analysing file: \"$file\""
+
+    # Read file line by line
+    while read -r line; do
+        # For each word in that line
+        for word in $line; do
+            # Regex for weblink
+            if [[ $word =~ $regex ]]; then
+                # Sleep so we don't spam the server
+                sleep "$sleep_time"
+
+                link="${BASH_REMATCH[0]}"
+                echo "Archiving link: '$link'"
+
+                # Perform curl request
+                response=$(curl -s -I "https://web.archive.org/save/$link")
+
+                # Get response code
+                response_code=$(echo "$response" | grep --ignore-case "HTTP/" | cut -d' ' -f2)
+
+                # Check response code
+                if [[ "$response_code" == "302" ]]; then
+                    # Success
+                    location=$(echo "$response" | grep --ignore-case "Location: https" | cut -d' ' -f2)
+
+                    echo "Archived to:   \"$location\""
+                elif [[ "$response_code" == "429" ]]; then
+                    # Too many requests
+                    echo "WARN: Sending too many requests!"
+                else
+                    # Unknown
+                    echo "WARN: Unknown error"
+                    echo "WARN: Response code: $response_code"
+                    echo "WARN: Full curl response"
+                    echo "$response"
+                fi
+            fi
+        done
+    done <"$file"
+done
+
+echo "Finished archiving!"
