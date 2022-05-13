da6b328b9e1da75fe43f62c770ea33c357537e98
AMS21 <AMS21.github@gmail.com>
2022-05-13 22:04:13 +0200

    [tests] Silence clang-tidy warnings about unhandled exceptions

--- tests/PhiCore/runtime_failure/src/selftest/unhandled_exception.fail.cpp
+++ tests/PhiCore/runtime_failure/src/selftest/unhandled_exception.fail.cpp
@@ -1,8 +1,10 @@
 #include "SetupRuntimeFailure.hpp"
 
+// NOLINTNEXTLINE(bugprone-exception-escape)
 int main()
 {
     SetupRuntimeFailure();
 
+    // NOLINTNEXTLINE(hicpp-exception-baseclass)
     throw "Exception which will not be caught";
 }
