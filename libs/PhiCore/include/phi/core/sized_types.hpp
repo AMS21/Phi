c89712246310a67f0ea8adf94c9de12639355375
AMS21 <AMS21.github@gmail.com>
2022-06-22 12:48:39 +0200

    [core] Add "reasonable" defaults for sized types

--- libs/PhiCore/include/phi/core/sized_types.hpp
+++ libs/PhiCore/include/phi/core/sized_types.hpp
@@ -35,19 +35,21 @@ namespace detail
     using signed_int8   = signed char;
     using unsigned_int8 = unsigned char;
 #elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 8
-    using signed_int8                        = signed short;
-    using unsigned int8                      = unsigned short;
+    using signed_int8    = signed short;
+    using unsigned int8  = unsigned short;
 #elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 8
-    using signed_int8                        = signed int;
-    using unsigned int8                      = unsigned int;
+    using signed_int8    = signed int;
+    using unsigned int8  = unsigned int;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 8
-    using signed_int8                        = signed long;
-    using unsigned_int8                      = unsigned long;
+    using signed_int8    = signed long;
+    using unsigned_int8  = unsigned long;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 8
     using signed_int8    = signed long long;
     using unsigned_int8  = unsigned long long;
 #else
-#    error "No integer of size 8 found!"
+    // Guess reasonable default
+    using signed_int8   = signed char;
+    using unsigned_int8 = unsigned char;
 #endif
 
     // int16
@@ -55,19 +57,21 @@ namespace detail
     using signed_int16   = signed char;
     using unsigned_int16 = unsigned char;
 #elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 16
-    using signed_int16                       = signed short;
-    using unsigned_int16                     = unsigned short;
+    using signed_int16   = signed short;
+    using unsigned_int16 = unsigned short;
 #elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 16
-    using signed_int16                       = signed int;
-    using unsigned int16                     = unsigned int;
+    using signed_int16   = signed int;
+    using unsigned int16 = unsigned int;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 16
-    using signed_int16                       = signed long;
-    using unsigned_int16                     = unsigned long;
+    using signed_int16   = signed long;
+    using unsigned_int16 = unsigned long;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 16
     using signed_int16   = signed long long;
     using unsigned_int16 = unsigned long long;
 #else
-#    error "No integer of size 16 found!"
+    // Guess reasonable default
+    using signed_int16   = signed short;
+    using unsigned_int16 = unsigned short;
 #endif
 
     // int32
@@ -75,19 +79,21 @@ namespace detail
     using signed_int32   = signed char;
     using unsigned_int32 = unsigned char;
 #elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 32
-    using signed_int32                       = signed short;
-    using unsigned_int32                     = unsigned short;
+    using signed_int32   = signed short;
+    using unsigned_int32 = unsigned short;
 #elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 32
-    using signed_int32                       = signed int;
-    using unsigned_int32                     = unsigned int;
+    using signed_int32   = signed int;
+    using unsigned_int32 = unsigned int;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 32
-    using signed_int32                       = signed long;
-    using unsigned_int32                     = unsigned long;
+    using signed_int32   = signed long;
+    using unsigned_int32 = unsigned long;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 32
     using signed_int32   = signed long long;
     using unsigned_int32 = unsigned long long;
 #else
-#    error "No integer of size 32 found!"
+    // Guess reasonable default
+    using signed_int32   = signed int;
+    using unsigned_int32 = unsigned int;
 #endif
 
     // int64
@@ -95,41 +101,45 @@ namespace detail
     using signed_int64   = signed char;
     using unsigned_int64 = unsigned char;
 #elif PHI_TYPE_SYSTEM_SIZEOF_SHORT() * CHAR_BIT == 64
-    using signed_int64                       = signed short;
-    using unsigned_int64                     = unsigned short;
+    using signed_int64   = signed short;
+    using unsigned_int64 = unsigned short;
 #elif PHI_TYPE_SYSTEM_SIZEOF_INT() * CHAR_BIT == 64
-    using signed_int64                       = signed int;
-    using unsigned_int64                     = unsigned int;
+    using signed_int64   = signed int;
+    using unsigned_int64 = unsigned int;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG() * CHAR_BIT == 64
-    using signed_int64                       = signed long;
-    using unsigned_int64                     = unsigned long;
+    using signed_int64   = signed long;
+    using unsigned_int64 = unsigned long;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG() * CHAR_BIT == 64
     using signed_int64   = signed long long;
     using unsigned_int64 = unsigned long long;
 #else
-#    error "No integer of size 64 found!"
+    // Guess reasonable default
+    using signed_int64   = signed long long;
+    using unsigned_int64 = unsigned long long;
 #endif
 
     // float32
 #if PHI_TYPE_SYSTEM_SIZEOF_FLOAT() * CHAR_BIT == 32
     using float32 = float;
 #elif PHI_TYPE_SYSTEM_SIZEOF_DOUBLE() * CHAR_BIT == 32
-    using float32                            = double;
+    using float32        = double;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG_DOUBLE() * CHAR_BIT == 32
-    using float32                            = long double;
+    using float32        = long double;
 #else
-#    error "No float of size 32 found!"
+    // Guess reasonable default
+    using float32 = float;
 #endif
 
     // float64
 #if PHI_TYPE_SYSTEM_SIZEOF_FLOAT() * CHAR_BIT == 64
     using float64 = float;
 #elif PHI_TYPE_SYSTEM_SIZEOF_DOUBLE() * CHAR_BIT == 64
-    using float64                            = double;
+    using float64        = double;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG_DOUBLE() * CHAR_BIT == 64
-    using float64                            = long double;
+    using float64        = long double;
 #else
-#    error "No float of size 64 found!"
+    // Guess reasonable default
+    using float64  = double;
 #endif
 
     PHI_GCC_SUPPRESS_WARNING_PUSH()
@@ -137,21 +147,20 @@ namespace detail
 
     // Optional float128
 #if PHI_TYPE_SYSTEM_SIZEOF_FLOAT() * CHAR_BIT == 128
-    static constexpr const bool has_float128 = true;
-    using float128                           = float;
+#    define PHI_TYPE_SYSTEM_HAS_FLOAT128() 1
+    using float128 = float;
 #elif PHI_TYPE_SYSTEM_SIZEOF_DOUBLE() * CHAR_BIT == 128
-    static constexpr const bool has_float128 = true;
-    using float128                           = double;
+#    define PHI_TYPE_SYSTEM_HAS_FLOAT128() 1
+    using float128       = double;
 #elif PHI_TYPE_SYSTEM_SIZEOF_LONG_DOUBLE() * CHAR_BIT == 128
-    static constexpr const bool has_float128 = true;
-    using float128                           = long double;
+#    define PHI_TYPE_SYSTEM_HAS_FLOAT128() 1
+    using float128       = long double;
 #else
-    static constexpr const bool has_float128 = false;
-    using float128                           = void;
+#    define PHI_TYPE_SYSTEM_HAS_FLOAT128() 0
+    using float128 = void;
 #endif
 
     PHI_GCC_SUPPRESS_WARNING_POP()
-
 } // namespace detail
 
 using int8_t  = detail::signed_int8;
@@ -197,6 +206,14 @@ using int_fast16_t = detail::signed_int64;
 using uint_fast16_t = detail::unsigned_int64;
 using int_fast32_t = detail::signed_int64;
 using uint_fast32_t = detail::unsigned_int64;
+using int_fast64_t = detail::signed_int64;
+using uint_fast64_t = detail::unsigned_int64;
+#else
+// Otherwise just use the exact size
+using int_fast16_t = detail::signed_int16;
+using uint_fast16_t = detail::unsigned_int16;
+using int_fast32_t = detail::signed_int32;
+using uint_fast32_t = detail::unsigned_int32;
 using int_fast64_t = detail::signed_int64;
 using uint_fast64_t = detail::unsigned_int64;
 #endif
