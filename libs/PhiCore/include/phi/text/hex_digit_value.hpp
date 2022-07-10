1639dc9655db6f056e5845b7f42553bcbcad5139
AMS21 <AMS21.github@gmail.com>
2022-07-10 15:31:33 +0200

    [Core] Optimize implementation of `hex_digit_value`

--- libs/PhiCore/include/phi/text/hex_digit_value.hpp
+++ libs/PhiCore/include/phi/text/hex_digit_value.hpp
@@ -7,13 +7,64 @@
 #    pragma once
 #endif
 
+#include "phi/compiler_support/warning.hpp"
 #include "phi/compiler_support/nodiscard.hpp"
 #include "phi/core/assert.hpp"
 #include "phi/core/types.hpp"
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
-// Implementation comparision: https://godbolt.org/z/PvTjobbvb
+// Implementation comparison: https://godbolt.org/z/TejhY4nYW
+// Stackoverflow https://stackoverflow.com/questions/34365746/whats-the-fastest-way-to-convert-hex-to-integer-in-c
+
+namespace detail
+{
+    PHI_CLANG_SUPPRESS_WARNING_PUSH()
+    PHI_CLANG_SUPPRESS_WARNING("-Wchar-subscripts")
+
+    struct hex_digit_value_lookup_table
+    {
+        constexpr hex_digit_value_lookup_table() noexcept
+            : m_Table{}
+        {
+            m_Table['0'] = 0;
+            m_Table['1'] = 1;
+            m_Table['2'] = 2;
+            m_Table['3'] = 3;
+            m_Table['4'] = 4;
+            m_Table['5'] = 5;
+            m_Table['6'] = 6;
+            m_Table['7'] = 7;
+            m_Table['8'] = 8;
+            m_Table['9'] = 9;
+            m_Table['a'] = 10;
+            m_Table['A'] = 10;
+            m_Table['b'] = 11;
+            m_Table['B'] = 11;
+            m_Table['c'] = 12;
+            m_Table['C'] = 12;
+            m_Table['d'] = 13;
+            m_Table['D'] = 13;
+            m_Table['e'] = 14;
+            m_Table['E'] = 14;
+            m_Table['f'] = 15;
+            m_Table['F'] = 15;
+        }
+
+        PHI_CLANG_SUPPRESS_WARNING_POP()
+
+        constexpr unsigned char operator[](char const index) const noexcept
+        {
+            // NOLINTNEXTLINE(bugprone-signed-char-misuse)
+            return static_cast<unsigned char>(m_Table[static_cast<size_t>(index)]);
+        }
+
+        private:
+            unsigned long long m_Table[103];
+    };
+
+    static constexpr hex_digit_value_lookup_table lookup_table;
+} // namespace detail
 
 PHI_NODISCARD constexpr u8 hex_digit_value(const char character) noexcept
 {
@@ -22,18 +73,7 @@ PHI_NODISCARD constexpr u8 hex_digit_value(const char character) noexcept
                            (character >= 'A' && character <= 'F'),
                    "Character out of bounds");
 
-    if (character >= '0' && character <= '9')
-    {
-        return static_cast<typename u8::value_type>(character - '0');
-    }
-
-    if (character >= 'a' && character <= 'f')
-    {
-        return static_cast<typename u8::value_type>(10 + character - 'a');
-    }
-
-    // character >= 'A' && character <= 'F'
-    return static_cast<typename u8::value_type>(10 + character - 'A');
+    return detail::lookup_table[character];
 }
 
 DETAIL_PHI_END_NAMESPACE()
