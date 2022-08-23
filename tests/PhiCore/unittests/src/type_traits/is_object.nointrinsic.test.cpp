3e7b0bccbc09216ed4a11ab729804d93fdb4f382
AMS21 <AMS21.github@gmail.com>
2022-08-23 08:17:40 +0200

    [tests] Improve tests for `is_object`

---
+++ tests/PhiCore/unittests/src/type_traits/is_object.nointrinsic.test.cpp
@@ -0,0 +1,3 @@
+#define PHI_SUPPORTS_IS_OBJECT() 0
+
+#include "is_object.test.cpp" // NOLINT(bugprone-suspicious-include)
---
+++ tests/PhiCore/unittests/src/type_traits/is_object.nointrinsic2.test.cpp
@@ -0,0 +1,4 @@
+#define PHI_SUPPORTS_IS_OBJECT() 0
+#define PHI_SUPPORTS_IS_UNION()  0
+
+#include "is_object.test.cpp" // NOLINT(bugprone-suspicious-include)
