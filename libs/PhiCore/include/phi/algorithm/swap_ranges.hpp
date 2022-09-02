7a803b60977033e069b821d819380c49ea665f92
AMS21 <AMS21.github@gmail.com>
2022-09-02 13:28:50 +0200

    [algorithm][tests] More workarounds for gcc-8

--- libs/PhiCore/include/phi/algorithm/swap_ranges.hpp
+++ libs/PhiCore/include/phi/algorithm/swap_ranges.hpp
@@ -8,16 +8,19 @@
 #endif
 
 #include "phi/algorithm/swap.hpp"
+#include "phi/compiler_support/compiler.hpp"
 #include "phi/compiler_support/constexpr.hpp"
 #include "phi/core/declval.hpp"
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
 template <typename LhsIteratorT, typename RhsIteratorT>
-PHI_EXTENDED_CONSTEXPR RhsIteratorT
-swap_ranges(LhsIteratorT lhs_first, LhsIteratorT lhs_last,
-            RhsIteratorT rhs_first) noexcept(noexcept(swap(*declval<LhsIteratorT>(),
-                                                           *declval<RhsIteratorT>())))
+PHI_EXTENDED_CONSTEXPR RhsIteratorT swap_ranges(LhsIteratorT lhs_first, LhsIteratorT lhs_last,
+                                                RhsIteratorT rhs_first)
+// TODO: This noexcept expression seems to not work on gcc-8
+#if PHI_COMPILER_IS_NOT(GCC) || PHI_COMPILER_IS_ATLEAST(GCC, 9, 0, 0)
+        noexcept(noexcept(swap(*declval<LhsIteratorT>(), *declval<RhsIteratorT>())))
+#endif
 {
     for (; lhs_first != lhs_last; ++lhs_first, (void)++rhs_first)
     {
