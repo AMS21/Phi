a7f23499f1578411958f0813f6c3a46414d886c9
AMS21 <AMS21.github@gmail.com>
2022-11-18 15:53:01 +0100

    [Tests] Some test improvements for `is_constant_evaluated`

--- tests/PhiCore/unittests/src/type_traits/is_constant_evaluated.test.cpp
+++ tests/PhiCore/unittests/src/type_traits/is_constant_evaluated.test.cpp
@@ -7,6 +7,22 @@ template <bool>
 struct InTemplate
 {};
 
+static constexpr bool constexpr_function() noexcept
+{
+    return phi::is_constant_evaluated();
+}
+
+#if PHI_HAS_FEATURE_CONSTEVAL()
+static consteval bool consteval_function() noexcept
+{
+    return phi::is_constant_evaluated();
+}
+#endif
+
+#if PHI_HAS_FEATURE_CONSTINIT()
+constinit const static bool constinit_value = phi::is_constant_evaluated();
+#endif
+
 TEST_CASE("is_constant_evaluated")
 {
     CHECK_SAME_TYPE(decltype(phi::is_constant_evaluated()), bool);
@@ -20,10 +36,35 @@ TEST_CASE("is_constant_evaluated")
     static_assert(phi::is_constant_evaluated(), "is_constant_evulated");
     CHECK_SAME_TYPE(InTemplate<phi::is_constant_evaluated()>, InTemplate<true>);
 
+    // Static initialization
     static int local_static = phi::is_constant_evaluated() ? 42 : -1;
     CHECK(local_static == 42);
 
+    // Constexpr initialization
     constexpr bool constexpr_val = phi::is_constant_evaluated();
     STATIC_REQUIRE(constexpr_val);
+
+    // Constexpr function evoked at runtime
+    bool constexpr_function_runtime_result = constexpr_function();
+    CHECK_FALSE(constexpr_function_runtime_result);
+
+    // Constexpr function evoked at compile time
+    constexpr bool constexpr_function_constexpr_result = constexpr_function();
+    STATIC_REQUIRE(constexpr_function_constexpr_result);
+
+    STATIC_REQUIRE(constexpr_function());
+
+    // Consteval function
+#    if PHI_HAS_FEATURE_CONSTEVAL()
+    bool consteval_function_result = consteval_function();
+    CHECK(consteval_function_result);
+
+    STATIC_REQUIRE(consteval_function());
+#    endif
+
+    // Constinit
+#    if PHI_HAS_FEATURE_CONSTINIT()
+    STATIC_REQUIRE(constinit_value);
+#    endif
 #endif
 }
