69662b3ec723a6f8d717a1e7f637e0c47673a62e
AMS21 <AMS21.github@gmail.com>
2022-11-27 14:53:41 +0100

    [tests] Add tests for `constexpr.hpp`

---
+++ tests/PhiCore/unittests/src/compiler_support/constexpr.test.cpp
@@ -0,0 +1,196 @@
+#include <phi/test/test_macros.hpp>
+
+#include <phi/algorithm/string_equals.hpp>
+#include <phi/compiler_support/constexpr.hpp>
+#include <phi/preprocessor/stringify.hpp>
+
+TEST_CASE("PHI_CONSTEXPR")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR), ""));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_OR_CONST")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR_CONST), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR_CONST), "const"));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_OR_INLINE")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR_INLINE), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR_INLINE), "inline"));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_OR_STATIC")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR_STATIC), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR_STATIC), "static"));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_OR")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR(blub)), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_OR(blub)), "blub"));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_IF")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_IF(0)), ""));
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_IF(1)), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_IF(0)), ""));
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_IF(1)), ""));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR), ""));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR_OR_CONST")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR_CONST), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR_CONST), "const"));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR_IR_INLINE")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(
+            phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR_INLINE), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR_INLINE), "inline"));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR_OR_STATIC")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(
+            phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR_STATIC), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR_STATIC), "static"));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR_OR")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR(blub)), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_OR(blub)), "blub"));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR_IF")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_IF(0)), ""));
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_IF(1)), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_IF(0)), ""));
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_IF(1)), ""));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_AND_CONST")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_AND_CONST), "constexpr const"));
+#elif PHI_HAS_FEATUREE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_AND_CONST), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_AND_CONST), ""));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_AND_CONST_OR")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(
+            phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_AND_CONST_OR(blub)), "constexpr const"));
+#elif PHI_HAS_FEATUREE_CONSTEXPR()
+    STATIC_REQUIRE(
+            phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_AND_CONST_OR(blub)), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_AND_CONST_OR(blub)), "blub"));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR_AND_CONST")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(
+            phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_AND_CONST), "constexpr const"));
+#elif PHI_HAS_FEATUREE_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_AND_CONST), ""));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_AND_CONST), ""));
+#endif
+}
+
+TEST_CASE("PHI_EXTENDED_CONSTEXPR_AND_CONST_OR")
+{
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_AND_CONST_OR(blub)),
+                                      "constexpr const"));
+#elif PHI_HAS_FEATUREE_CONSTEXPR()
+    STATIC_REQUIRE(
+            phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_AND_CONST_OR(blub)), "blub"));
+#else
+    STATIC_REQUIRE(
+            phi::string_equals(PHI_STRINGIFY(PHI_EXTENDED_CONSTEXPR_AND_CONST_OR(blub)), "blub"));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_LAMBDA")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR_LAMBDA()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_LAMBDA), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_LAMBDA), ""));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_VIRTUAL")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR_VIRTUAL()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_VIRTUAL), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_VIRTUAL), ""));
+#endif
+}
+
+TEST_CASE("PHI_CONSTEXPR_DESTRUCTOR")
+{
+#if PHI_HAS_FEATURE_CONSTEXPR_DESTRUCTOR()
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_DESTRUCTOR), "constexpr"));
+#else
+    STATIC_REQUIRE(phi::string_equals(PHI_STRINGIFY(PHI_CONSTEXPR_DESTRUCTOR), ""));
+#endif
+}
