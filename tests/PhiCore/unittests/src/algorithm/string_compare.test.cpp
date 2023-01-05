042457606317b86e21a3445b9d5cb264ca3eff59
AMS21 <AMS21.github@gmail.com>
2023-01-05 16:35:03 +0100

    [Tests] Use extended constexpr where needed

--- tests/PhiCore/unittests/src/algorithm/string_compare.test.cpp
+++ tests/PhiCore/unittests/src/algorithm/string_compare.test.cpp
@@ -27,33 +27,34 @@ TEST_CASE("string_compare - char*/nullptr_t")
 
 TEST_CASE("string_compare - char*/char*")
 {
-    STATIC_REQUIRE(phi::string_compare(char_nullptr, char_nullptr) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("", char_nullptr) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare(char_nullptr, "") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("", "") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare(char_nullptr, char_nullptr) ==
+                       phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("", char_nullptr) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare(char_nullptr, "") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("", "") == phi::CompareResult::Equal);
 
-    STATIC_REQUIRE(phi::string_compare("a", "") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("", "a") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("", "a") == phi::CompareResult::LessThan);
 
-    STATIC_REQUIRE(phi::string_compare(char_nullptr, "a") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("a", char_nullptr) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("a", "a") == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("a", "b") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("a", "c") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("b", "a") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("c", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare(char_nullptr, "a") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", char_nullptr) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "a") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "b") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "c") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("b", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("c", "a") == phi::CompareResult::GreaterThan);
 
-    STATIC_REQUIRE(phi::string_compare("ab", "ab") == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("ab", "a") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("a", "ab") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "ab") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "ab") == phi::CompareResult::LessThan);
 
-    STATIC_REQUIRE(phi::string_compare("abc", "abc") == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("abc", "ab") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("abc", "a") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("abc", "") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("ab", "abc") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("a", "abc") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("", "abc") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("abc", "abc") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("abc", "ab") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("abc", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("abc", "") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "abc") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "abc") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("", "abc") == phi::CompareResult::LessThan);
 }
 
 TEST_CASE("string_compare with size - nullptr_t/nullptr_t")
@@ -78,35 +79,37 @@ TEST_CASE("string_compare with size - char*/nullptr_t")
 
 TEST_CASE("string_compare with size - char*/char*")
 {
-    STATIC_REQUIRE(phi::string_compare(char_nullptr, char_nullptr, 21u) ==
-                   phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare(char_nullptr, char_nullptr, 1u) ==
-                   phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("", char_nullptr, 21u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare(char_nullptr, "", 21u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("", "", 21u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare(char_nullptr, char_nullptr, 21u) ==
+                       phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare(char_nullptr, char_nullptr, 1u) ==
+                       phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("", char_nullptr, 21u) ==
+                       phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare(char_nullptr, "", 21u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("", "", 21u) == phi::CompareResult::Equal);
 
-    STATIC_REQUIRE(phi::string_compare("a", "", 1u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("", "a", 1u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "", 1u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("", "a", 1u) == phi::CompareResult::LessThan);
 
-    STATIC_REQUIRE(phi::string_compare(char_nullptr, "a", 1u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("a", char_nullptr, 1u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("a", "a", 1u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("a", "a", 2u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("a", "a", 21u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("a", "b", 1u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("a", "c", 1u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("b", "a", 1u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("c", "a", 1u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare(char_nullptr, "a", 1u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", char_nullptr, 1u) ==
+                       phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "a", 1u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "a", 2u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "a", 21u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "b", 1u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "c", 1u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("b", "a", 1u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("c", "a", 1u) == phi::CompareResult::GreaterThan);
 
-    STATIC_REQUIRE(phi::string_compare("ab", "ab", 2u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("ab", "ab", 1u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("ab", "ab", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("ab", "af", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("ab", "a", 5u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("ab", "a", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("a", "ab", 5u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare("a", "ab", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare("f", "ab", 0u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare("ab", "f", 0u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "ab", 2u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "ab", 1u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "ab", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "af", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "a", 5u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "a", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "ab", 5u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("a", "ab", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare("f", "ab", 0u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare("ab", "f", 0u) == phi::CompareResult::LessThan);
 }
--- tests/PhiCore/unittests/src/algorithm/string_compare_unsafe.test.cpp
+++ tests/PhiCore/unittests/src/algorithm/string_compare_unsafe.test.cpp
@@ -6,53 +6,55 @@
 
 TEST_CASE("string_compare_unsafe - char*/char*")
 {
-    STATIC_REQUIRE(phi::string_compare_unsafe("", "") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("", "") == phi::CompareResult::Equal);
 
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("", "a") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("", "a") == phi::CompareResult::LessThan);
 
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "a") == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "b") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "c") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("b", "a") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("c", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "a") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "b") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "c") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("b", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("c", "a") == phi::CompareResult::GreaterThan);
 
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab") == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "a") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "ab") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "ab") == phi::CompareResult::LessThan);
 
-    STATIC_REQUIRE(phi::string_compare_unsafe("abc", "abc") == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("abc", "ab") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("abc", "a") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("abc", "") == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "abc") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "abc") == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("", "abc") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("abc", "abc") == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("abc", "ab") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("abc", "a") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("abc", "") == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "abc") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "abc") == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("", "abc") == phi::CompareResult::LessThan);
 }
 
 TEST_CASE("string_compare_unsafe with size - char*/char*")
 {
-    STATIC_REQUIRE(phi::string_compare_unsafe("", "", 21u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("", "", 21u) == phi::CompareResult::Equal);
 
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "", 1u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("", "a", 1u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "", 1u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("", "a", 1u) == phi::CompareResult::LessThan);
 
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "a", 1u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "a", 2u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "a", 21u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "b", 1u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "c", 1u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("b", "a", 1u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("c", "a", 1u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "a", 1u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "a", 2u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "a", 21u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "b", 1u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "c", 1u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("b", "a", 1u) == phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("c", "a", 1u) == phi::CompareResult::GreaterThan);
 
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab", 2u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab", 1u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "af", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "a", 5u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "a", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "ab", 5u) == phi::CompareResult::LessThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("a", "ab", 0u) == phi::CompareResult::Equal);
-    STATIC_REQUIRE(phi::string_compare_unsafe("f", "ab", 0u) == phi::CompareResult::GreaterThan);
-    STATIC_REQUIRE(phi::string_compare_unsafe("ab", "f", 0u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab", 2u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab", 1u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "ab", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "af", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "a", 5u) ==
+                       phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "a", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "ab", 5u) == phi::CompareResult::LessThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("a", "ab", 0u) == phi::CompareResult::Equal);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("f", "ab", 0u) ==
+                       phi::CompareResult::GreaterThan);
+    EXT_STATIC_REQUIRE(phi::string_compare_unsafe("ab", "f", 0u) == phi::CompareResult::LessThan);
 }
