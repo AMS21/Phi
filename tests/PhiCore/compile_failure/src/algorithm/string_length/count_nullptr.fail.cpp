e64d6e4e0b2aeb6e6851a6a743fa35e52a655df8
AMS21 <AMS21.github@gmail.com>
2023-01-06 14:14:35 +0100

    [Tests] Fix faulty compile time failure test

--- tests/PhiCore/compile_failure/src/algorithm/string_length/count_nullptr.fail.cpp
+++
@@ -1,6 +0,0 @@
-#include <phi/algorithm/string_length.hpp>
-
-int main()
-{
-    phi::usize l = phi::string_length(nullptr, 0u);
-}
---
+++ tests/PhiCore/compile_failure/src/algorithm/string_length_unsafe/nullptr.fail.cpp
@@ -0,0 +1,6 @@
+#include <phi/algorithm/string_length_unsafe.hpp>
+
+int main()
+{
+    (void)phi::string_length_unsafe(nullptr);
+}
