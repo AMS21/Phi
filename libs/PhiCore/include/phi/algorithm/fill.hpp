72652eefa100050f1366b594e40b16d485d96b24
AMS21 <AMS21.github@gmail.com>
2023-01-26 18:04:38 +0100

    [algorithm] Implement `find`

---
+++ libs/PhiCore/include/phi/algorithm/fill.hpp
@@ -0,0 +1,29 @@
+#ifndef INCG_PHI_CORE_ALGORITHM_FILL_HPP
+#define INCG_PHI_CORE_ALGORITHM_FILL_HPP
+
+#include "phi/phi_config.hpp"
+
+#if PHI_HAS_EXTENSION_PRAGMA_ONCE()
+#    pragma once
+#endif
+
+#include "phi/compiler_support/constexpr.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+template <typename ForwardIteratorT, typename TypeT>
+PHI_EXTENDED_CONSTEXPR void fill(
+        ForwardIteratorT first, ForwardIteratorT last,
+        const TypeT& value) noexcept(noexcept(first !=
+                                              last) && noexcept(++first) && noexcept(*first =
+                                                                                             value))
+{
+    for (; first != last; ++first)
+    {
+        *first = value;
+    }
+}
+
+DETAIL_PHI_END_NAMESPACE()
+
+#endif // INCG_PHI_CORE_ALGORITHM_FILL_HPP
---
+++ tests/PhiCore/unittests/src/algorithm/fill.test.cpp
@@ -0,0 +1,53 @@
+#include <phi/test/test_macros.hpp>
+
+#include <phi/algorithm/at.hpp>
+#include <phi/algorithm/fill.hpp>
+#include <phi/compiler_support/constexpr.hpp>
+#include <phi/compiler_support/warning.hpp>
+#include <phi/container/array.hpp>
+#include <phi/core/assert.hpp>
+#include <phi/iterator/begin.hpp>
+#include <phi/iterator/end.hpp>
+
+PHI_GCC_SUPPRESS_WARNING("-Wsuggest-attribute=pure")
+
+PHI_EXTENDED_CONSTEXPR bool test_fill_constexpr()
+{
+    int array[3]{0, 1, 2};
+
+    phi::fill(phi::begin(array), phi::end(array), -1);
+
+    PHI_RELEASE_ASSERT(phi::at(array, 0u) == -1);
+    PHI_RELEASE_ASSERT(phi::at(array, 1u) == -1);
+    PHI_RELEASE_ASSERT(phi::at(array, 2u) == -1);
+
+    return true;
+}
+
+TEST_CASE("algorithm.fill - c-array")
+{
+    int array[3]{0, 1, 2};
+
+    phi::fill(phi::begin(array), phi::end(array), -1);
+    CHECK_NOEXCEPT(phi::fill(phi::begin(array), phi::end(array), -1));
+
+    CHECK(phi::at(array, 0u) == -1);
+    CHECK(phi::at(array, 1u) == -1);
+    CHECK(phi::at(array, 2u) == -1);
+
+#if PHI_HAS_FEATURE_EXTENDED_CONSTEXPR()
+    test_fill_constexpr();
+#endif
+}
+
+TEST_CASE("algorithm.fill - phi::array")
+{
+    phi::array<int, 3u> array{0, 1, 2};
+
+    phi::fill(phi::begin(array), phi::end(array), -1);
+    CHECK_NOEXCEPT(phi::fill(phi::begin(array), phi::end(array), -1));
+
+    CHECK(phi::at(array, 0u) == -1);
+    CHECK(phi::at(array, 1u) == -1);
+    CHECK(phi::at(array, 2u) == -1);
+}
