5f13a8d473c19bed94cb069f40e64a123a259eb0
AMS21 <AMS21.github@gmail.com>
2023-02-11 16:57:32 +0100

    [type_system] Implement `max`

---
+++ libs/PhiCore/include/phi/type_system/max.hpp
@@ -0,0 +1,262 @@
+#ifndef INCG_PHI_CORE_TYPE_SYSTEM_MAX_HPP
+#define INCG_PHI_CORE_TYPE_SYSTEM_MAX_HPP
+
+#include "phi/phi_config.hpp"
+
+#if PHI_HAS_EXTENSION_PRAGMA_ONCE()
+#    pragma once
+#endif
+
+#include "phi/compiler_support/nodiscard.hpp"
+#include "phi/core/size_t.hpp"
+#include "phi/generated/compiler_support/type_system.hpp"
+#include "phi/type_traits/false_t.hpp"
+#include "phi/type_traits/make_unsafe.hpp"
+#include "phi/type_traits/remove_cv.hpp"
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+namespace type_system
+{
+    namespace detail
+    {
+        template <typename TypeT, size_t Size>
+        struct signed_int_max_impl;
+
+        template <typename TypeT>
+        struct signed_int_max_impl<TypeT, 1u>
+        {
+            static constexpr TypeT value = TypeT(127LL);
+        };
+
+        template <typename TypeT>
+        struct signed_int_max_impl<TypeT, 2u>
+        {
+            static constexpr TypeT value = TypeT(32767LL);
+        };
+
+        template <typename TypeT>
+        struct signed_int_max_impl<TypeT, 4u>
+        {
+            static constexpr TypeT value = TypeT(2147483647LL);
+        };
+
+        template <typename TypeT>
+        struct signed_int_max_impl<TypeT, 8u>
+        {
+            static constexpr TypeT value = TypeT(9223372036854775807LL);
+        };
+
+        template <typename TypeT, size_t Size>
+        struct unsigned_int_max_impl;
+
+        template <typename TypeT>
+        struct unsigned_int_max_impl<TypeT, 1u>
+        {
+            static constexpr TypeT value = TypeT(255ULL);
+        };
+
+        template <typename TypeT>
+        struct unsigned_int_max_impl<TypeT, 2u>
+        {
+            static constexpr TypeT value = TypeT(65535ULL);
+        };
+
+        template <typename TypeT>
+        struct unsigned_int_max_impl<TypeT, 4u>
+        {
+            static constexpr TypeT value = TypeT(4294967295ULL);
+        };
+
+        template <typename TypeT>
+        struct unsigned_int_max_impl<TypeT, 8u>
+        {
+            static constexpr TypeT value = TypeT(18446744073709551615ULL);
+        };
+
+        template <typename TypeT, size_t Size>
+        struct float_max_impl;
+
+        template <typename TypeT>
+        struct float_max_impl<TypeT, 4u>
+        {
+            static constexpr TypeT value = TypeT(3.4028234e+38f);
+        };
+
+        template <typename TypeT>
+        struct float_max_impl<TypeT, 8u>
+        {
+            static constexpr TypeT value = TypeT(1.7976931348623157e+308);
+        };
+
+#if PHI_HAS_EXTENSION_FLOAT128_LITERALS()
+
+        template <typename TypeT>
+        struct float_max_impl<TypeT, 16u>
+        {
+#    if PHI_COMPILER_IS(EMCC)
+            static constexpr TypeT value = TypeT(1.18973149535723176508575932662800702e+4932L);
+#    else
+            static constexpr TypeT value = TypeT(1.18973149535723176502e+4932L);
+#    endif
+        };
+
+#endif
+
+        // Base implementation
+        template <typename TypeT>
+        PHI_NODISCARD constexpr TypeT max_impl() noexcept
+        {
+            static_assert(false_t<TypeT>::value, "No specilization for the given type found.");
+            return TypeT{};
+        }
+
+        template <>
+        PHI_NODISCARD constexpr bool max_impl<bool>() noexcept
+        {
+            return true;
+        }
+
+        // Character types
+
+        template <>
+        PHI_NODISCARD constexpr char max_impl<char>() noexcept
+        {
+#if PHI_TYPE_SYSTEM_CHAR_SIGNED()
+            return detail::signed_int_max_impl<char, PHI_TYPE_SYSTEM_SIZEOF_CHAR()>::value;
+#else
+            return detail::unsigned_int_max_impl<char, PHI_TYPE_SYSTEM_SIZEOF_CHAR()>::value;
+#endif
+        }
+
+        template <>
+        PHI_NODISCARD constexpr wchar_t max_impl<wchar_t>() noexcept
+        {
+#if PHI_TYPE_SYSTEM_WCHAR_T_SIGNED()
+            return detail::signed_int_max_impl<wchar_t, PHI_TYPE_SYSTEM_SIZEOF_WCHAR_T()>::value;
+#else
+            return detail::unsigned_int_max_impl<wchar_t, PHI_TYPE_SYSTEM_SIZEOF_WCHAR_T()>::value;
+#endif
+        }
+
+#if PHI_HAS_FEATURE_CHAR8_T()
+
+        template <>
+        PHI_NODISCARD constexpr char8_t max_impl<char8_t>() noexcept
+        {
+            return detail::unsigned_int_max_impl<char8_t, 1u>::value;
+        }
+
+#endif
+
+        template <>
+        PHI_NODISCARD constexpr char16_t max_impl<char16_t>() noexcept
+        {
+            return detail::unsigned_int_max_impl<char16_t, 2u>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr char32_t max_impl<char32_t>() noexcept
+        {
+            return detail::unsigned_int_max_impl<char32_t, 4u>::value;
+        }
+
+        // Signed integer type
+        template <>
+        PHI_NODISCARD constexpr signed char max_impl<signed char>() noexcept
+        {
+            return detail::signed_int_max_impl<signed char, PHI_TYPE_SYSTEM_SIZEOF_CHAR()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr short max_impl<short>() noexcept
+        {
+            return detail::signed_int_max_impl<short, PHI_TYPE_SYSTEM_SIZEOF_SHORT()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr int max_impl<int>() noexcept
+        {
+            return detail::signed_int_max_impl<int, PHI_TYPE_SYSTEM_SIZEOF_INT()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr long max_impl<long>() noexcept
+        {
+            return detail::signed_int_max_impl<long, PHI_TYPE_SYSTEM_SIZEOF_LONG()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr long long max_impl<long long>() noexcept
+        {
+            return detail::signed_int_max_impl<long long,
+                                               PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG()>::value;
+        }
+
+        // Unsigned integer types
+
+        template <>
+        PHI_NODISCARD constexpr unsigned char max_impl<unsigned char>() noexcept
+        {
+            return detail::unsigned_int_max_impl<unsigned char,
+                                                 PHI_TYPE_SYSTEM_SIZEOF_CHAR()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr unsigned short max_impl<unsigned short>() noexcept
+        {
+            return detail::unsigned_int_max_impl<unsigned short,
+                                                 PHI_TYPE_SYSTEM_SIZEOF_SHORT()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr unsigned max_impl<unsigned>() noexcept
+        {
+            return detail::unsigned_int_max_impl<unsigned, PHI_TYPE_SYSTEM_SIZEOF_INT()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr unsigned long max_impl<unsigned long>() noexcept
+        {
+            return detail::unsigned_int_max_impl<unsigned long,
+                                                 PHI_TYPE_SYSTEM_SIZEOF_LONG()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr unsigned long long max_impl<unsigned long long>() noexcept
+        {
+            return detail::unsigned_int_max_impl<unsigned long long,
+                                                 PHI_TYPE_SYSTEM_SIZEOF_LONG_LONG()>::value;
+        }
+
+        // Floating point numbers
+
+        template <>
+        PHI_NODISCARD constexpr float max_impl<float>() noexcept
+        {
+            return detail::float_max_impl<float, PHI_TYPE_SYSTEM_SIZEOF_FLOAT()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr double max_impl<double>() noexcept
+        {
+            return detail::float_max_impl<double, PHI_TYPE_SYSTEM_SIZEOF_DOUBLE()>::value;
+        }
+
+        template <>
+        PHI_NODISCARD constexpr long double max_impl<long double>() noexcept
+        {
+            return detail::float_max_impl<long double, PHI_TYPE_SYSTEM_SIZEOF_LONG_DOUBLE()>::value;
+        }
+    } // namespace detail
+
+    template <typename TypeT>
+    PHI_NODISCARD constexpr remove_cv_t<TypeT> max() noexcept
+    {
+        return detail::max_impl<remove_cv_t<make_unsafe_t<TypeT>>>();
+    }
+} // namespace type_system
+
+DETAIL_PHI_END_NAMESPACE()
+
+#endif // INCG_PHI_CORE_TYPE_SYSTEM_MAX_HPP
---
+++ tests/PhiCore/unittests/src/type_system/max.test.cpp
@@ -0,0 +1,107 @@
+#include <phi/test/test_macros.hpp>
+
+#include <phi/compiler_support/char8_t.hpp>
+#include <phi/compiler_support/warning.hpp>
+#include <phi/core/boolean.hpp>
+#include <phi/core/floating_point.hpp>
+#include <phi/core/integer.hpp>
+#include <phi/type_system/max.hpp>
+#include <limits>
+
+template <typename TypeT>
+void type_system_max_std_impl()
+{
+    PHI_CLANG_AND_GCC_SUPPRESS_WARNING_WITH_PUSH("-Wfloat-equal")
+
+    STATIC_REQUIRE(phi::type_system::max<TypeT>() == std::numeric_limits<TypeT>::max());
+    CHECK(phi::type_system::max<TypeT>() == std::numeric_limits<TypeT>::max());
+
+    CHECK_NOEXCEPT(phi::type_system::max<TypeT>());
+
+    PHI_CLANG_AND_GCC_SUPPRESS_WARNING_POP()
+}
+
+template <typename TypeT>
+void type_system_max_std()
+{
+    type_system_max_std_impl<TypeT>();
+    type_system_max_std_impl<const TypeT>();
+    type_system_max_std_impl<volatile TypeT>();
+    type_system_max_std_impl<const volatile TypeT>();
+}
+
+template <typename TypeT>
+void type_system_max_safe_impl()
+{
+    PHI_CLANG_AND_GCC_SUPPRESS_WARNING_WITH_PUSH("-Wfloat-equal")
+
+    STATIC_REQUIRE(phi::type_system::max<TypeT>().unsafe() ==
+                   phi::type_system::max<typename TypeT::value_type>());
+    CHECK(phi::type_system::max<TypeT>().unsafe() ==
+          phi::type_system::max<typename TypeT::value_type>());
+
+    CHECK_NOEXCEPT(phi::type_system::max<TypeT>());
+
+    PHI_CLANG_AND_GCC_SUPPRESS_WARNING_POP()
+}
+
+template <typename TypeT>
+void type_system_max_safe()
+{
+    type_system_max_safe_impl<TypeT>();
+    type_system_max_safe_impl<const TypeT>();
+    type_system_max_safe_impl<volatile TypeT>();
+    type_system_max_safe_impl<const volatile TypeT>();
+}
+
+TEST_CASE("type_system.max")
+{
+    type_system_max_std<bool>();
+
+    // char types
+    type_system_max_std<char>();
+    type_system_max_std<wchar_t>();
+    type_system_max_std<char8_t>();
+    type_system_max_std<char16_t>();
+    type_system_max_std<char32_t>();
+
+    // Signed integer types
+    type_system_max_std<signed char>();
+    type_system_max_std<short>();
+    type_system_max_std<int>();
+    type_system_max_std<long>();
+    type_system_max_std<long long>();
+
+    // Unsigned integer types
+    type_system_max_std<unsigned char>();
+    type_system_max_std<unsigned short>();
+    type_system_max_std<unsigned>();
+    type_system_max_std<unsigned long>();
+    type_system_max_std<unsigned long long>();
+
+    // floating point types
+    type_system_max_std<float>();
+    type_system_max_std<double>();
+    type_system_max_std<long double>();
+    //static_assert(__LDBL_MAX__ < 0.0f, "");
+
+    // Safe types
+
+    type_system_max_safe<phi::boolean>();
+
+    type_system_max_safe<phi::integer<signed char>>();
+    type_system_max_safe<phi::integer<short>>();
+    type_system_max_safe<phi::integer<int>>();
+    type_system_max_safe<phi::integer<long>>();
+    type_system_max_safe<phi::integer<long long>>();
+
+    type_system_max_safe<phi::integer<unsigned char>>();
+    type_system_max_safe<phi::integer<unsigned short>>();
+    type_system_max_safe<phi::integer<unsigned>>();
+    type_system_max_safe<phi::integer<unsigned long>>();
+    type_system_max_safe<phi::integer<unsigned long long>>();
+
+    type_system_max_safe<phi::floating_point<float>>();
+    type_system_max_safe<phi::floating_point<double>>();
+    type_system_max_safe<phi::floating_point<long double>>();
+}
