25d37911251652ae9a10eb7a278a37ec2b5b75d1
AMS21 <AMS21.github@gmail.com>
2023-02-25 08:54:33 +0100

    [tests] Improve tests for `digit_value`

--- tests/PhiCore/unittests/src/text/digit_value.test.cpp
+++ tests/PhiCore/unittests/src/text/digit_value.test.cpp
@@ -2,7 +2,7 @@
 
 #include <phi/text/digit_value.hpp>
 
-TEST_CASE("digit_value")
+TEST_CASE("digit_value - constexpr")
 {
     STATIC_REQUIRE(phi::digit_value('0').unsafe() == 0u);
     STATIC_REQUIRE(phi::digit_value('1').unsafe() == 1u);
@@ -14,4 +14,20 @@ TEST_CASE("digit_value")
     STATIC_REQUIRE(phi::digit_value('7').unsafe() == 7u);
     STATIC_REQUIRE(phi::digit_value('8').unsafe() == 8u);
     STATIC_REQUIRE(phi::digit_value('9').unsafe() == 9u);
+
+    CHECK_NOEXCEPT(phi::digit_value('0'));
+}
+
+TEST_CASE("digit_value - runtime")
+{
+    CHECK(phi::digit_value('0').unsafe() == 0u);
+    CHECK(phi::digit_value('1').unsafe() == 1u);
+    CHECK(phi::digit_value('2').unsafe() == 2u);
+    CHECK(phi::digit_value('3').unsafe() == 3u);
+    CHECK(phi::digit_value('4').unsafe() == 4u);
+    CHECK(phi::digit_value('5').unsafe() == 5u);
+    CHECK(phi::digit_value('6').unsafe() == 6u);
+    CHECK(phi::digit_value('7').unsafe() == 7u);
+    CHECK(phi::digit_value('8').unsafe() == 8u);
+    CHECK(phi::digit_value('9').unsafe() == 9u);
 }
