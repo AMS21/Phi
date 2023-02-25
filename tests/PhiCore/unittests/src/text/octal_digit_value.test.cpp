4671cd6d898922b2ed6934e9b090f566194d5fa2
AMS21 <AMS21.github@gmail.com>
2023-02-25 08:56:44 +0100

    [tests] Improve tests for `octal_digit_value`

--- tests/PhiCore/unittests/src/text/octal_digit_value.test.cpp
+++ tests/PhiCore/unittests/src/text/octal_digit_value.test.cpp
@@ -2,7 +2,7 @@
 
 #include <phi/text/octal_digit_value.hpp>
 
-TEST_CASE("octal_digit_value")
+TEST_CASE("octal_digit_value - constexpr")
 {
     STATIC_REQUIRE(phi::octal_digit_value('0').unsafe() == 0u);
     STATIC_REQUIRE(phi::octal_digit_value('1').unsafe() == 1u);
@@ -12,4 +12,18 @@ TEST_CASE("octal_digit_value")
     STATIC_REQUIRE(phi::octal_digit_value('5').unsafe() == 5u);
     STATIC_REQUIRE(phi::octal_digit_value('6').unsafe() == 6u);
     STATIC_REQUIRE(phi::octal_digit_value('7').unsafe() == 7u);
+
+    CHECK_NOEXCEPT(phi::octal_digit_value('0'));
+}
+
+TEST_CASE("octal_digit_value - runtime")
+{
+    CHECK(phi::octal_digit_value('0').unsafe() == 0u);
+    CHECK(phi::octal_digit_value('1').unsafe() == 1u);
+    CHECK(phi::octal_digit_value('2').unsafe() == 2u);
+    CHECK(phi::octal_digit_value('3').unsafe() == 3u);
+    CHECK(phi::octal_digit_value('4').unsafe() == 4u);
+    CHECK(phi::octal_digit_value('5').unsafe() == 5u);
+    CHECK(phi::octal_digit_value('6').unsafe() == 6u);
+    CHECK(phi::octal_digit_value('7').unsafe() == 7u);
 }
