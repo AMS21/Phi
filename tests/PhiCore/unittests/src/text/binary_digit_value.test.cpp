c84be43796c6c680690204d1492384879f6eb53c
AMS21 <AMS21.github@gmail.com>
2023-02-25 08:55:46 +0100

    [tests] Improve tests for `binary_digit_value`

--- tests/PhiCore/unittests/src/text/binary_digit_value.test.cpp
+++ tests/PhiCore/unittests/src/text/binary_digit_value.test.cpp
@@ -2,8 +2,16 @@
 
 #include <phi/text/binary_digit_value.hpp>
 
-TEST_CASE("binary_digit_value")
+TEST_CASE("binary_digit_value - constexpr")
 {
     STATIC_REQUIRE(phi::binary_digit_value('0').unsafe() == 0u);
     STATIC_REQUIRE(phi::binary_digit_value('1').unsafe() == 1u);
+
+    CHECK_NOEXCEPT(phi::binary_digit_value('0'));
+}
+
+TEST_CASE("binary_digit_value - runtime")
+{
+    CHECK(phi::binary_digit_value('0').unsafe() == 0u);
+    CHECK(phi::binary_digit_value('1').unsafe() == 1u);
 }
