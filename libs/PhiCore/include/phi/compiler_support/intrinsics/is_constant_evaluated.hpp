0ac35a61e76afe66c202722edcfa0b958e04cb32
AMS21 <AMS21.github@gmail.com>
2023-04-05 20:34:56 +0200

    [compiler_support] `__builtin_is_constant_evaluated` is supported with clang-9

--- libs/PhiCore/include/phi/compiler_support/intrinsics/is_constant_evaluated.hpp
+++ libs/PhiCore/include/phi/compiler_support/intrinsics/is_constant_evaluated.hpp
@@ -11,7 +11,7 @@
 
 #if !defined(PHI_IS_CONSTANT_EVALUATED) && !defined(PHI_SUPPORTS_IS_CONSTANT_EVALUATED)
 
-#    if PHI_COMPILER_IS_ATLEAST(GCC, 9, 0, 0) || PHI_COMPILER_IS_ATLEAST(CLANG, 8, 0, 0) ||        \
+#    if PHI_COMPILER_IS_ATLEAST(GCC, 9, 0, 0) || PHI_COMPILER_IS_ATLEAST(CLANG, 9, 0, 0) ||        \
             PHI_COMPILER_IS(APPLECLANG) || PHI_COMPILER_IS(EMCC) ||                                \
             PHI_COMPILER_IS_ATLEAST(MSVC, 19, 29, 0)
 #        define PHI_IS_CONSTANT_EVALUATED()          __builtin_is_constant_evaluated()
