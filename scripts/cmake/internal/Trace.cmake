cf4afbdf8362d06fe7722257ea0f186b28090a82
AMS21 <AMS21.github@gmail.com>
2023-04-08 22:02:19 +0200

    [CMake] Remove `PHI_VERBOSE_CMAKE`

--- scripts/cmake/internal/Trace.cmake
+++ scripts/cmake/internal/Trace.cmake
@@ -4,7 +4,7 @@ include(internal/Message)
 
 # Log a trace message
 function(phi_trace text)
-  if(PHI_VERBOSE_CMAKE)
+  if(${PHI_MAIN_PROJECT})
     phi_message(STATUS "[Phi]: ${text}")
   endif()
 endfunction()
