3f0f8b3fab5e4cbbefaa6842a0be9ca5028c216f
AMS21 <AMS21.github@gmail.com>
2023-04-12 21:19:41 +0200

    [algorithm] Add some doxygen comments and fix `all_of` algorithm

--- libs/PhiCore/include/phi/algorithm/accumulate.hpp
+++ libs/PhiCore/include/phi/algorithm/accumulate.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file accumulate.hpp
+ * @brief Contains the implementation of the accumulate algorithm from the C++ standard library.
+ *
+ * @see https://en.cppreference.com/w/cpp/algorithm/accumulate
+ */                                                                                                \
 #ifndef INCG_PHI_CORE_ALGORITHM_ACCUMULATE_HPP
 #define INCG_PHI_CORE_ALGORITHM_ACCUMULATE_HPP
 
@@ -13,6 +19,21 @@
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
+/**
+ * @brief Calculates the sum of values in the range [first, last), starting with the initial value `init`.
+ *
+ * This function performs a left fold on the input range.
+ *
+ * @tparam InputIteratorT The iterator type of the input range.
+ * @tparam TypeT The type of the result.
+ * @param first The first iterator of the input range.
+ * @param last The end iterator of the input range.
+ * @param init The initial value to start the sum.
+ * @return The sum of all the values in the range.
+ *
+ * @note This function uses move semantics to avoid unnecessary copying of values.
+ * @see https://en.cppreference.com/w/cpp/algorithm/accumulate
+ */
 // TODO: noexcept
 template <typename InputIteratorT, typename TypeT>
 PHI_NODISCARD PHI_EXTENDED_CONSTEXPR TypeT accumulate(InputIteratorT first, InputIteratorT last,
@@ -27,6 +48,24 @@ PHI_NODISCARD PHI_EXTENDED_CONSTEXPR TypeT accumulate(InputIteratorT first, Inpu
 }
 
 // TODO: noexcept
+/**
+ * @brief Calculates the sum of values in the range [first, last), using the binary operation `binary_operator`
+ * to accumulate the values.
+ *
+ * This function performs a left fold on the input range.
+ *
+ * @tparam InputIteratorT The iterator type of the input range.
+ * @tparam TypeT The type of the result.
+ * @tparam BinaryOperationT The type of the binary operator used to accumulate the values.
+ * @param first The first iterator of the input range.
+ * @param last The end iterator of the input range.
+ * @param init The initial value to start the sum.
+ * @param binary_operator The binary operator used to accumulate the values.
+ * @return The accumulated value of all the values in range.
+ *
+ * @note This function uses move semantics to avoid unnecessary copying of values.
+ * @see https://en.cppreference.com/w/cpp/algorithm/accumulate
+ */
 template <typename InputIteratorT, typename TypeT, typename BinaryOperationT>
 PHI_NODISCARD PHI_EXTENDED_CONSTEXPR TypeT accumulate(InputIteratorT first, InputIteratorT last,
                                                       TypeT init, BinaryOperationT binary_operator)
--- libs/PhiCore/include/phi/algorithm/all_of.hpp
+++ libs/PhiCore/include/phi/algorithm/all_of.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file all_of.hpp
+ * @brief Contains the implementation of the all_of algorithm from the C++ standard library.
+ *
+ * @see https://en.cppreference.com/w/cpp/algorithm/all_of
+ */
 #ifndef INCG_PHI_CORE_ALGORITHM_ALL_OF_HPP
 #define INCG_PHI_CORE_ALGORITHM_ALL_OF_HPP
 
@@ -13,13 +19,25 @@
 DETAIL_PHI_BEGIN_NAMESPACE()
 
 // TODO: noexcept
+/**
+ * @brief Returns true if all elements in the range [first, last) satisfy the predicate.
+ *
+ * @tparam InputIteratorT The iterator type of the input range.
+ * @tparam PredicateT The type of the predicate function.
+ * @param first The first iterator of the input range.
+ * @param last The end iterator of the input range.
+ * @param predicate The unary predicate function which should return true for the elements to be included in the result.
+ * @return `true` if the predicate returns `true` for all elements in the range, `false` otherwise.
+ *
+ * @see https://en.cppreference.com/w/cpp/algorithm/all_of
+ */
 template <typename InputIteratorT, typename PredicateT>
 PHI_NODISCARD PHI_EXTENDED_CONSTEXPR bool all_of(InputIteratorT first, InputIteratorT last,
                                                  PredicateT predicate)
 {
     for (; first != last; ++first)
     {
-        if (predicate(*first))
+        if (!predicate(*first))
         {
             return false;
         }
