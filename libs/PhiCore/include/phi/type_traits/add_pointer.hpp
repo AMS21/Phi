f5885e62cde7d601e5c81d31c446e6e13ff13569
AMS21 <AMS21.github@gmail.com>
2023-06-01 17:23:59 +0200

    [compiler_support] Add support for `add_pointer` intrinsic

--- libs/PhiCore/include/phi/type_traits/add_pointer.hpp
+++ libs/PhiCore/include/phi/type_traits/add_pointer.hpp
@@ -7,8 +7,25 @@
 #    pragma once
 #endif
 
-#include "phi/type_traits/remove_reference.hpp"
-#include "phi/type_traits/type_identity.hpp"
+#include "phi/compiler_support/intrinsics/add_pointer.hpp"
+
+#if PHI_SUPPORTS_ADD_POINTER()
+
+DETAIL_PHI_BEGIN_NAMESPACE()
+
+template <typename TypeT>
+struct add_pointer
+{
+    using type = PHI_ADD_POINTER(TypeT);
+};
+
+template <typename TypeT>
+using add_pointer_t = PHI_ADD_POINTER(TypeT);
+
+#else
+
+#    include "phi/type_traits/remove_reference.hpp"
+#    include "phi/type_traits/type_identity.hpp"
 
 DETAIL_PHI_BEGIN_NAMESPACE()
 
@@ -28,6 +45,8 @@ struct add_pointer : public decltype(detail::try_add_pointer<TypeT>(0))
 
 template <typename TypeT>
 using add_pointer_t = typename add_pointer<TypeT>::type;
+
+#endif
 
 DETAIL_PHI_END_NAMESPACE()
 
---
+++ tests/PhiCore/unittests/src/type_traits/add_pointer.nointrinsic.test.cpp
@@ -0,0 +1,3 @@
+#define PHI_SUPPORTS_ADD_POINTER() 0
+
+#include "add_pointer.test.cpp" // NOLINT(bugprone-suspicious-include)
