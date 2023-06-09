e3e4d21dfd1a28e0b617266dc75a7bb4fc275a82
AMS21 <AMS21.github@gmail.com>
2023-06-09 14:42:37 +0200

    [compiler_support] Improve `standard_library.hpp`
    - Provide access to the version and date of the standard library in use
    (if supported)
    - Also in general cleaner and more robust code

--- libs/PhiCore/include/phi/compiler_support/standard_library.hpp
+++ libs/PhiCore/include/phi/compiler_support/standard_library.hpp
@@ -8,6 +8,7 @@
 #endif
 
 #include <phi/generated/compiler_support/libraries.hpp>
+#include <phi/preprocessor/versioning.hpp>
 
 // Check if the standard library has the <version> header
 #if PHI_HAS_LIB_VERSION()
@@ -25,45 +26,85 @@
 
 #endif
 
-// Check explicit standards first
-#if defined(PHI_CONFIG_STDLIB_LIBCXX)
-#    define PHI_STANDARD_LIBRARY_LIBCXX() 1
-#elif defined(PHI_CONFIG_STDLIB_LIBSTDCXX)
-#    define PHI_STANDARD_LIBRARY_LIBSTDCXX() 1
-#else
+#define PHI_STANDARD_LIBRARY_LIBCXX()    0
+#define PHI_STANDARD_LIBRARY_LIBSTDCXX() 0
+#define PHI_STANDARD_LIBRARY_MSVC()      0
+
 // Check default standard library
+#if defined(_LIBCPP_VERSION)
 
-#    if defined(__GLIBCXX__)
-#        define PHI_STANDARD_LIBRARY_LIBSTDCXX() 1
-#    elif defined(_LIBCPP_VERSION)
-#        define PHI_STANDARD_LIBRARY_LIBCXX() 1
-#    elif defined(_MSVC_STL_VERSION)
-#        define PHI_STANDARD_LIBRARY_MSVC() 1
+#    undef PHI_STANDARD_LIBRARY_LIBCXX
+#    define PHI_STANDARD_LIBRARY_LIBCXX() 1
+#    define PHI_STANDARD_LIBRARY_NAME()   "libc++"
+
+// NOTE: libc++ does not provide a date
+#    define PHI_STANDARD_LIBRARY_DATE() (0)
+
+#    define PHI_STANDARD_LIBRARY_VERSION_MAJOR() (_LIBCPP_VERSION >> 4)
+#    define PHI_STANDARD_LIBRARY_VERSION_MINOR() (_LIBCPP_VERSION >> 2 & 0xFF)
+#    define PHI_STANDARD_LIBRARY_VERSION_PATCH() (_LIBCPP_VERSION & 0xFF)
+
+#elif defined(__GLIBCXX__) || defined(_GLIBCXX_RELEASE)
+
+#    undef PHI_STANDARD_LIBRARY_LIBSTDCXX
+#    define PHI_STANDARD_LIBRARY_LIBSTDCXX() 1
+#    define PHI_STANDARD_LIBRARY_NAME()      "libstdc++"
+
+// Date format: YYYYMMDD
+#    define PHI_STANDARD_LIBRARY_DATE()      (__GLIBCXX__)
+
+// NOTE: Not all version of libstdc++ provide the _GLIBCXX_RELEASE macro and it only contains the major version
+#    if defined(_GLIBCXX_RELEASE)
+#        define PHI_STANDARD_LIBRARY_VERSION_MAJOR() (_GLIBCXX_RELEASE)
+#        define PHI_STANDARD_LIBRARY_VERSION_MINOR() (0)
+#        define PHI_STANDARD_LIBRARY_VERSION_PATCH() (0)
 #    else
-#        error "Unable to determine default standard library"
+#        define PHI_STANDARD_LIBRARY_VERSION_MAJOR() (0)
+#        define PHI_STANDARD_LIBRARY_VERSION_MINOR() (0)
+#        define PHI_STANDARD_LIBRARY_VERSION_PATCH() (0)
 #    endif
-#endif
 
-#if !defined(PHI_STANDARD_LIBRARY_LIBSTDCXX)
-#    define PHI_STANDARD_LIBRARY_LIBSTDCXX() 0
-#endif
+#elif defined(_CPPLIB_VER) || defined(_MSVC_STL_VERSION) || defined(_MSVC_STL_UPDATE)
 
-#if !defined(PHI_STANDARD_LIBRARY_LIBCXX)
-#    define PHI_STANDARD_LIBRARY_LIBCXX() 0
-#endif
-
-#if !defined(PHI_STANDARD_LIBRARY_MSVC)
-#    define PHI_STANDARD_LIBRARY_MSVC() 0
-#endif
-
-// Standard library name
-#if PHI_STANDARD_LIBRARY_LIBCXX()
-#    define PHI_STANDARD_LIBRARY_NAME() "libc++"
-#elif PHI_STANDARD_LIBRARY_LIBSTDCXX()
-#    define PHI_STANDARD_LIBRARY_NAME() "libstdc++"
-#elif PHI_STANDARD_LIBRARY_MSVC()
+#    undef PHI_STANDARD_LIBRARY_MSVC
+#    define PHI_STANDARD_LIBRARY_MSVC() 1
 #    define PHI_STANDARD_LIBRARY_NAME() "msvc-stl"
+
+// NOTE: The _MSVC_STL_UPDATE macro was added with Visual Studio 2017 15.5
+// See: https://github.com/microsoft/STL/wiki/Macro-_MSVC_STL_UPDATE
+#    if defined(_MSVC_STL_UPDATE)
+
+// Date format: YYYYMM
+#        define PHI_STANDARD_LIBRARY_DATE() (_MSVC_STL_UPDATE)
+
+#    else
+
+#        define PHI_STANDARD_LIBRARY_DATE() (0)
+
+#    endif
+
+// NOTE: The version seems to match the msvc version output (PHI_MSVC_VERSION not _MSC_VER) but without the patch part
+//       See Environment.cmake for more about the MSVC/VS version madness.
+#    define PHI_STANDARD_LIBRARY_VERSION_MAJOR() (_MSVC_STL_VERSION / 10)
+#    define PHI_STANDARD_LIBRARY_VERSION_MINOR() (_MSVC_STL_VERSION % 10)
+#    define PHI_STANDARD_LIBRARY_VERSION_PATCH() (0)
+
+#else
+
+// Probably unsupported standard library used
+#    error "Unable to determine default standard library"
+
 #endif
+
+#if PHI_STANDARD_LIBRARY_DATE() > 0
+#    define PHI_STANDARD_LIBRARY_HAS_DATE() 1
+#else
+#    define PHI_STANDARD_LIBRARY_HAS_DATE() 0
+#endif
+
+#define PHI_STANDARD_LIBRARY_VERSION()                                                             \
+    PHI_VERSION_CREATE(PHI_STANDARD_LIBRARY_VERSION_MAJOR(), PHI_STANDARD_LIBRARY_VERSION_MINOR(), \
+                       PHI_STANDARD_LIBRARY_VERSION_PATCH())
 
 // Define macros for opening and closing the std namespace
 #if PHI_STANDARD_LIBRARY_LIBCXX()
