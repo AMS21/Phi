50a1d54c3aca73521c8f0aad6f7f47c54eb2d05a
AMS21 <AMS21.github@gmail.com>
2024-03-05 22:54:11 +0100

    [tests] Fix weird mingw build failure in debug mode

--- tests/PhiCore/unittests/src/forward/std/pair.test.cpp
+++ tests/PhiCore/unittests/src/forward/std/pair.test.cpp
@@ -1,5 +1,6 @@
 #include <phi/test/test_macros.hpp>
 
+#include <phi/compiler_support/compiler.hpp>
 #include <phi/forward/std/pair.hpp>
 
 TEST_CASE("forward.std.pair.forward")
@@ -9,9 +10,16 @@ TEST_CASE("forward.std.pair.forward")
     CHECK(pointer == nullptr);
 }
 
+// TODO: For some reason Mingw builds with '_GLIBCXX_DEBUG' defined produce the following compile error
+// utility:212:10: error: '__glibcxx_assert_fail' is not a member of 'std'
+// So we just undefine it to avoid this error
+#if PHI_COMPILER_IS(MINGW) && defined(_GLIBCXX_DEBUG)
+#    undef _GLIBCXX_DEBUG
+#endif
+
 #include <utility>
 
-TEST_CASE("forward.std.forward")
+TEST_CASE("forward.std.pair")
 {
     std::pair<int, int> pair;
 
