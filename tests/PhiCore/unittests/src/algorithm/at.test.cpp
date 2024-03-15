4e4784319bd1e7ad8fe165681c9ace17cdee8465
AMS21 <AMS21.github@gmail.com>
2024-03-15 16:28:07 +0100

    [tests] Test with our own containers instead of relying on standard ones

--- tests/PhiCore/unittests/src/algorithm/at.test.cpp
+++ tests/PhiCore/unittests/src/algorithm/at.test.cpp
@@ -3,10 +3,8 @@
 #include "constexpr_helper.hpp"
 #include <phi/algorithm/at.hpp>
 #include <phi/compiler_support/constexpr.hpp>
-#include <array>
-#include <deque>
+#include <phi/container/array.hpp>
 #include <initializer_list>
-#include <vector>
 
 TEST_CASE("At c-array", "[Utility][At]")
 {
@@ -41,38 +39,13 @@ TEST_CASE("At c-array", "[Utility][At]")
 
 TEST_CASE("At container", "[Utility][At]")
 {
-    SECTION("std::vector")
-    {
-        std::vector<int> vec{0, 1, 2, 3, 4};
+    phi::array<int, 5> arr{5, 9, 8, 12, 21};
 
-        CHECK(phi::at(vec, 0) == 0);
-        CHECK(phi::at(vec, 1) == 1);
-        CHECK(phi::at(vec, 2) == 2);
-        CHECK(phi::at(vec, 3) == 3);
-        CHECK(phi::at(vec, 4) == 4);
-    }
-
-    SECTION("std::array")
-    {
-        std::array<int, 5> arr{5, 9, 8, 12, 21};
-
-        CHECK(phi::at(arr, 0) == 5);
-        CHECK(phi::at(arr, 1) == 9);
-        CHECK(phi::at(arr, 2) == 8);
-        CHECK(phi::at(arr, 3) == 12);
-        CHECK(phi::at(arr, 4) == 21);
-    }
-
-    SECTION("std::deque")
-    {
-        std::deque<int> queue{17, -5, 3, 1, 4};
-
-        CHECK(phi::at(queue, 0) == 17);
-        CHECK(phi::at(queue, 1) == -5);
-        CHECK(phi::at(queue, 2) == 3);
-        CHECK(phi::at(queue, 3) == 1);
-        CHECK(phi::at(queue, 4) == 4);
-    }
+    CHECK(phi::at(arr, 0) == 5);
+    CHECK(phi::at(arr, 1) == 9);
+    CHECK(phi::at(arr, 2) == 8);
+    CHECK(phi::at(arr, 3) == 12);
+    CHECK(phi::at(arr, 4) == 21);
 }
 
 TEST_CASE("At initializer_list", "[Utility][At]")
