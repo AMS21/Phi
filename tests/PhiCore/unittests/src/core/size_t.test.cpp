77eef724a6fd4ef0b3380c544fea8c1989887681
AMS21 <AMS21.github@gmail.com>
2024-03-18 09:57:30 +0100

    [tests] Add check that `size_t` is at least 16 bits with C++-11

--- tests/PhiCore/unittests/src/core/size_t.test.cpp
+++ tests/PhiCore/unittests/src/core/size_t.test.cpp
@@ -1,9 +1,11 @@
 #include <phi/test/test_macros.hpp>
 
+#include <phi/compiler_support/cpp_standard.hpp>
 #include <phi/core/size_t.hpp>
 #include <phi/type_traits/is_integral.hpp>
 #include <phi/type_traits/is_signed.hpp>
 #include <phi/type_traits/is_unsigned.hpp>
+#include <climits> // CHAR_BIT
 #include <cstddef> // std::size_t
 #include <type_traits>
 
@@ -19,6 +21,11 @@ TEST_CASE("size_t")
 
     STATIC_REQUIRE(sizeof(phi::size_t) == sizeof(std::size_t));
     STATIC_REQUIRE(sizeof(phi::size_t) == sizeof(void*));
+    // Since C++-11 'size_t' is at least 16 bits
+#if PHI_CPP_STANDARD_IS_ATLEAST(11)
+    STATIC_REQUIRE(sizeof(phi::size_t) * CHAR_BIT >= 16);
+#endif
+
     STATIC_REQUIRE(phi::is_unsigned<phi::size_t>::value);
     STATIC_REQUIRE_FALSE(phi::is_signed<phi::size_t>::value);
     STATIC_REQUIRE(phi::is_integral<phi::size_t>::value);
