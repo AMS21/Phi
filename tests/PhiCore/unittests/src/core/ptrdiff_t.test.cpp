bd95c6a710af2f03b4086f736c7ea28350601043
AMS21 <AMS21.github@gmail.com>
2024-03-18 10:05:24 +0100

    [tests] Add check that `ptrdiff_t` is at least 16 bits with C++-11

--- tests/PhiCore/unittests/src/core/ptrdiff_t.test.cpp
+++ tests/PhiCore/unittests/src/core/ptrdiff_t.test.cpp
@@ -1,8 +1,10 @@
 #include <phi/test/test_macros.hpp>
 
+#include <phi/compiler_support/cpp_standard.hpp>
 #include <phi/core/ptrdiff_t.hpp>
 #include <phi/type_traits/is_signed.hpp>
 #include <phi/type_traits/is_unsafe_integral.hpp>
+#include <climits> // CHAR_BIT
 #include <cstddef>
 
 TEST_CASE("ptrdiff_t")
@@ -12,6 +14,11 @@ TEST_CASE("ptrdiff_t")
     STATIC_REQUIRE(phi::is_unsafe_integral<phi::ptrdiff_t>::value);
 #if defined(__PTRDIFF_TYPE__)
     CHECK_SAME_TYPE(phi::ptrdiff_t, __PTRDIFF_TYPE__);
+#endif
+
+    // Since C++-11 'ptrdiff_t' is at least 16 bits
+#if PHI_CPP_STANDARD_IS_ATLEAST(11)
+    STATIC_REQUIRE(sizeof(phi::ptrdiff_t) * CHAR_BIT >= 16);
 #endif
 
     // Standard compatibility
