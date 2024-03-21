d4c1917764b3b92ed672106c37b05a726916401a
AMS21 <AMS21.github@gmail.com>
2024-03-21 09:44:36 +0100

    [Misc] Remove useless include

--- libs/PhiCore/include/phi/type_traits/disjunction.hpp
+++ libs/PhiCore/include/phi/type_traits/disjunction.hpp
@@ -10,7 +10,6 @@
 #include "phi/compiler_support/constexpr.hpp"
 #include "phi/compiler_support/inline_variables.hpp"
 #include "phi/compiler_support/warning.hpp"
-#include "phi/type_traits/conjunction.hpp"
 #include "phi/type_traits/integral_constant.hpp"
 
 PHI_MSVC_SUPPRESS_WARNING_PUSH()
